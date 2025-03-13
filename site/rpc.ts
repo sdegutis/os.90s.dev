74b04cf7aa24a0136b22ae2c930dceff88e5344e
sdegutis <sbdegutis@gmail.com>
2025-03-12 23:37:37 -0500

    Clean.

--- site/rpc.ts
+++ site/rpc.ts
@@ -25,8 +25,8 @@ type EventMap<T> = { [K in keyof T]: (...args: any) => void }
 
 export function wRPC<In extends EventMap<In>, Out extends EventMap<Out>>(port: Worker | Window, handlers: In) {
   port.onmessage = (msg) => {
-    const name = msg.data.pop()
-    handlers[name as keyof typeof handlers](...msg.data)
+    const name = msg.data.pop() as keyof typeof handlers
+    handlers[name](...msg.data)
   }
   return <K extends keyof Out>(name: K, data: Parameters<Out[K]>, transfer?: Transferable[]) => {
     port.postMessage([...data, name], transfer ? { transfer } : undefined)
