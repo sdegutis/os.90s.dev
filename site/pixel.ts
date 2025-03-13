0676aeefaf7ad67b27773e804cd7605dc586206f
sdegutis <sbdegutis@gmail.com>
2025-03-13 13:35:06 -0500

    Cleanup.

---
+++ site/pixel.ts
@@ -0,0 +1,27 @@
+import type { View } from "./controls/view.js"
+
+export class PixelCanvas {
+
+  view
+
+  private ctx!: OffscreenCanvasRenderingContext2D
+  private imgdata!: ImageData
+  pixels!: Uint8ClampedArray
+
+  constructor(view: View) {
+    this.view = view
+    this.rebuild()
+    this.view.resized.watch(() => this.rebuild())
+  }
+
+  private rebuild() {
+    this.ctx = this.view.canvas.getContext('2d')!
+    this.pixels = new Uint8ClampedArray(this.view.w * this.view.h * 4)
+    this.imgdata = new ImageData(this.pixels, this.view.w, this.view.h)
+  }
+
+  blit() {
+    this.ctx.putImageData(this.imgdata, 0, 0)
+  }
+
+}
