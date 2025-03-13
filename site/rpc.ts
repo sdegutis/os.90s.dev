6d393dba9796ba07550a27e001ee51ea553a97cc
sdegutis <sbdegutis@gmail.com>
2025-03-12 20:51:53 -0500

    Not great.

--- site/rpc.ts
+++ site/rpc.ts
@@ -32,9 +32,7 @@ type Payload<T extends EventMap<T>, K extends keyof T> =
 
 type EventMap<T> = { [K in keyof T]: (...args: any) => any }
 
-export function wRPC<
-  In extends EventMap<In>, Out extends EventMap<Out>
->(out: Out, port: Worker | Window, handlers: In) {
+export function wRPC<In extends EventMap<In>, Out extends EventMap<Out>>(out: Out, port: Worker | Window, handlers: In) {
   const promises = new Map<number, (data: any) => any>()
   let lastid = 0
 
@@ -43,7 +41,10 @@ export function wRPC<
     if (pkg.type === 'req') {
       const { id, name } = pkg
       const data = handlers[name as keyof typeof handlers](...pkg.data)
-      if (id !== -1) port.postMessage({ data, id, type: 'res' } as PayloadRes<any, any>)
+      if (id !== -1) {
+        const payload: PayloadRes<any, any> = { data, id, type: 'res' }
+        port.postMessage(payload)
+      }
     }
     else {
       const res = promises.get(pkg.id)
@@ -52,14 +53,20 @@ export function wRPC<
     }
   }
 
+  type ProxiedOutFn = {
+    [K in keyof Out]: (...args: [...Parameters<Out[K]>, tr?: Transferable[]]) => Promise<ReturnType<Out[K]>>
+  }
+
   return Object.fromEntries(Object.entries<() => any>(out).map(([name, fn]) => {
     const returns = fn()
-    return [name, <K extends keyof Out>(...data: Parameters<Out[K]>): Promise<ReturnType<Out[K]>> => {
+    return [name, <K extends keyof Out>(...data: [...Parameters<Out[K]>, tr?: Transferable[]]): Promise<ReturnType<Out[K]>> => {
+      const transfer = ((data.length === fn.length + 1) ? data.pop() : undefined) as Transferable[]
       let id = -1
       const p = Promise.withResolvers<ReturnType<Out[K]>>()
       if (returns) promises.set(id = lastid++, p.resolve)
-      port.postMessage({ id, name, data, type: 'req' } as PayloadReq<Out, any>)
+      const payload: PayloadReq<Out, any> = { id, name, data: data as any, type: 'req' }
+      port.postMessage(payload, { transfer })
       return p.promise
     }]
-  })) as { [K in keyof Out]: (...args: Parameters<Out[K]>) => Promise<ReturnType<Out[K]>> }
+  })) as ProxiedOutFn
 }
