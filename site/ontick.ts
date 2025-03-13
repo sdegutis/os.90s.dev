ed21cce28a8f913ab32cd0418392a9225890f58a
sdegutis <sbdegutis@gmail.com>
2025-03-13 10:36:29 -0500

    Cleanup.

---
+++ site/ontick.ts
@@ -0,0 +1,13 @@
+export function ontick(fn: (d: number) => void, fps = 30) {
+  let done: number
+  let last = performance.now();
+  (function tick(now: number) {
+    const delta = now - last
+    if (delta + 1 >= 1000 / fps) {
+      last = now
+      fn(delta)
+    }
+    done = requestAnimationFrame(tick)
+  })(last)
+  return () => cancelAnimationFrame(done)
+}
