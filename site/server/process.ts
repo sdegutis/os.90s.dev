ef1e460ad7a58850f4398603885ec107b8b18145
sdegutis <sbdegutis@gmail.com>
2025-03-14 11:08:27 -0500

    Cleanup.

--- site/server/process.ts
+++ site/server/process.ts
@@ -8,27 +8,22 @@ export class Process {
   static id = 0
   id
 
-  sys
-  rpc
-
   constructor(sys: Sys, path: string) {
-    this.sys = sys
-
     Process.all.set(this.id = ++Process.id, this)
 
     const absurl = new URL(path, import.meta.url)
     const worker = new Worker(absurl, { type: 'module' })
 
-    this.rpc = wRPC<ServerSys, ClientProg>(worker)
+    const rpc = wRPC<ServerSys, ClientProg>(worker)
 
-    this.rpc.once('init').then(() => {
-      this.rpc.send('init', [this.id])
+    rpc.once('init').then(() => {
+      rpc.send('init', [this.id])
     })
 
-    this.rpc.listen('newpanel', (pos) => {
+    rpc.listen('newpanel', (pos) => {
       const chan = new MessageChannel()
       const p = new Panel(chan.port1, pos)
-      this.rpc.send('newpanel', [p.id, p.x, p.y, p.w, p.h, chan.port2], [chan.port2])
+      rpc.send('newpanel', [p.id, p.x, p.y, p.w, p.h, chan.port2], [chan.port2])
 
       p.didAdjust.watch(() => sys.redrawAllPanels())
       p.didRedraw.watch(() => sys.redrawAllPanels())
