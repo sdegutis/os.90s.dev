ad770e8527e9c6b54ab4c454465fc30797c8d01f
sdegutis <sbdegutis@gmail.com>
2025-03-15 21:22:42 -0500

    ts

--- site/test.ts
+++ site/test.ts
@@ -1,25 +1,46 @@
-const GRID_W = 320 * 1;
-const GRID_H = 180 * 1;
+const GRID_W = 320
+const GRID_H = 180
 
-const canvas = /** @type {HTMLCanvasElement} */(document.querySelector("canvas"))
+
+const canvas = document.querySelector("canvas")!
+
 canvas.width = GRID_W
 canvas.height = GRID_H
-canvas.style.transform = `scale(3)`
+
 canvas.style.imageRendering = 'pixelated'
 canvas.style.backgroundColor = '#000'
 canvas.style.outline = 'none'
 canvas.style.cursor = 'none'
 
-const adapter = /** @type {GPUAdapter} */(await navigator.gpu.requestAdapter())
-const device = await adapter.requestDevice();
+document.body.replaceChildren(canvas)
+
+canvas.tabIndex = 1
+canvas.focus()
+
+new ResizeObserver(() => {
+  const rect = canvas.parentElement!.getBoundingClientRect()
+  let w = GRID_W, h = GRID_H, s = 1
+  while (
+    (w += GRID_W) <= rect.width &&
+    (h += GRID_H) <= rect.height) s++
+  canvas.style.transform = `scale(${s})`
+}).observe(canvas.parentElement!)
+
+
+
+
+
+const adapter = (await navigator.gpu.requestAdapter())!
+const device = await adapter.requestDevice()
 
 // Canvas configuration
-const context = /** @type {GPUCanvasContext} */(canvas.getContext("webgpu"))
-const canvasFormat = navigator.gpu.getPreferredCanvasFormat();
+const context = canvas.getContext('webgpu')!
+const canvasFormat = navigator.gpu.getPreferredCanvasFormat()
 context.configure({
   device: device,
   format: canvasFormat,
-});
+  alphaMode: 'premultiplied',
+})
 
 
 
@@ -27,34 +48,34 @@ context.configure({
 
 
 // Create an array representing the active state of each cell.
-const cellStateArray = new Uint32Array(GRID_W * GRID_H);
+const cellStateArray = new Uint32Array(GRID_W * GRID_H)
 
 // Create a storage buffer to hold the cell state.
 const cellStateStorage = device.createBuffer({
   label: "Cell State",
   size: cellStateArray.byteLength,
   usage: GPUBufferUsage.STORAGE | GPUBufferUsage.COPY_DST,
-});
+})
 
 
 
 
 const n = 0.8
-const vertices = new Float32Array([-n, -n, n, -n, n, n, n, n, -n, n, -n, -n]);
+const vertices = new Float32Array([-n, -n, n, -n, n, n, n, n, -n, n, -n, -n])
 
 const vertexBuffer = device.createBuffer({
   label: "Cell vertices",
   size: vertices.byteLength,
   usage: GPUBufferUsage.VERTEX | GPUBufferUsage.COPY_DST,
-});
+})
 
-device.queue.writeBuffer(vertexBuffer, 0, vertices);
+device.queue.writeBuffer(vertexBuffer, 0, vertices)
 
 
 const cellShaderModule = device.createShaderModule({
   label: "Cell shader",
-  code: await fetch('./vshader.wgsl').then(r => r.text())
-});
+  code: await fetch('/vshader.wgsl').then(r => r.text())
+})
 
 
 const cellPipeline = device.createRenderPipeline({
@@ -79,7 +100,7 @@ const cellPipeline = device.createRenderPipeline({
       format: canvasFormat
     }]
   }
-});
+})
 
 
 
@@ -87,13 +108,13 @@ const cellPipeline = device.createRenderPipeline({
 
 
 // Create a uniform buffer that describes the grid.
-const uniformArray = new Float32Array([GRID_W, GRID_H]);
+const uniformArray = new Float32Array([GRID_W, GRID_H])
 const uniformBuffer = device.createBuffer({
   label: "Grid Uniforms",
   size: uniformArray.byteLength,
   usage: GPUBufferUsage.UNIFORM | GPUBufferUsage.COPY_DST,
-});
-device.queue.writeBuffer(uniformBuffer, 0, uniformArray);
+})
+device.queue.writeBuffer(uniformBuffer, 0, uniformArray)
 
 const bindGroup = device.createBindGroup({
   label: "Cell renderer bind group",
@@ -105,7 +126,7 @@ const bindGroup = device.createBindGroup({
     binding: 1,
     resource: { buffer: cellStateStorage }
   }],
-});
+})
 
 
 let s = 0
@@ -113,36 +134,39 @@ let s = 0
 function render() {
 
   cellStateArray.fill(0)
-  for (let i = s++; i < cellStateArray.length; i += 6) {
-    cellStateArray[i] = Math.floor(Math.random() * 0xffffffff);
+  for (let i = s++; i < cellStateArray.length; i += 21) {
+    cellStateArray[i] = Math.floor(Math.random() * 0xffffffff)
     // cellStateArray[i] = 0x330000ff;
   }
-  device.queue.writeBuffer(cellStateStorage, 0, cellStateArray);
+  device.queue.writeBuffer(cellStateStorage, 0, cellStateArray)
 
 
 
   // Clear the canvas with a render pass
-  const encoder = device.createCommandEncoder();
+  const encoder = device.createCommandEncoder()
 
   const pass = encoder.beginRenderPass({
     colorAttachments: [{
       view: context.getCurrentTexture().createView(),
       loadOp: "clear",
+      clearValue: [0, 0, 0, 0],
       storeOp: "store",
     }]
-  });
+  })
 
 
-  pass.setPipeline(cellPipeline);
-  pass.setVertexBuffer(0, vertexBuffer);
-  pass.setBindGroup(0, bindGroup);
-  pass.draw(vertices.length / 2, GRID_W * GRID_H); // 6 vertices
+  pass.setPipeline(cellPipeline)
+  pass.setVertexBuffer(0, vertexBuffer)
+  pass.setBindGroup(0, bindGroup)
+  pass.draw(vertices.length / 2, GRID_W * GRID_H) // 6 vertices
 
-  pass.end();
+  pass.end()
 
 
-  device.queue.submit([(encoder.finish())]);
+  device.queue.submit([(encoder.finish())])
+
+  console.log('here')
 }
 
 render()
-// setInterval(render, 1000)
+setInterval(render, 1000)
