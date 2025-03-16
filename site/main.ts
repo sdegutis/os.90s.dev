3f02167b5120f39b3cbad11df2091ff929096f4e
sdegutis <sbdegutis@gmail.com>
2025-03-16 11:28:16 -0500

    Wait what

--- site/main.ts
+++ site/main.ts
@@ -1,12 +1,10 @@
 import { Process } from './server/process.js'
 import { Sys } from './server/sys.js'
 
-if (['localhost', '90s.dev'].includes(location.hostname)) {
-  const sys = new Sys(320, 180)
+const sys = new Sys(320, 180)
 
-  const shell = new Process(sys, '/apps/shell.js')
+const shell = new Process(sys, '/apps/shell.js')
 
-  const proc1 = new Process(sys, '/apps/prog1.js')
-  const proc2 = new Process(sys, '/apps/prog2.js')
-  const proc3 = new Process(sys, '/apps/prog3.js')
-}
+const proc1 = new Process(sys, '/apps/prog1.js')
+const proc2 = new Process(sys, '/apps/prog2.js')
+const proc3 = new Process(sys, '/apps/prog3.js')
