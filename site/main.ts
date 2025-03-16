9dd2184b38180b10a5ea500054d209b688ec2d88
sdegutis <sbdegutis@gmail.com>
2025-03-16 11:25:26 -0500

    Only here.

--- site/main.ts
+++ site/main.ts
@@ -1,16 +1,12 @@
 import { Process } from './server/process.js'
 import { Sys } from './server/sys.js'
 
-console.log(location)
+if (['localhost', '90s.dev'].includes(location.hostname)) {
+  const sys = new Sys(320, 180)
 
-const sys = new Sys(320, 180)
+  const shell = new Process(sys, '/apps/shell.js')
 
-const shell = new Process(sys, '/apps/shell.js')
-
-const proc1 = new Process(sys, '/apps/prog1.js')
-const proc2 = new Process(sys, '/apps/prog2.js')
-const proc3 = new Process(sys, '/apps/prog3.js')
-
-// setTimeout(() => {
-//   proc2.terminate()
-// }, 1000)
+  const proc1 = new Process(sys, '/apps/prog1.js')
+  const proc2 = new Process(sys, '/apps/prog2.js')
+  const proc3 = new Process(sys, '/apps/prog3.js')
+}
