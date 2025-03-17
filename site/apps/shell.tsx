eb10e0cc865f4333a681804510b51e552a5fd087
sdegutis <sbdegutis@gmail.com>
2025-03-17 11:27:50 -0500

    Resized.

--- site/apps/shell.tsx
+++ site/apps/shell.tsx
@@ -6,7 +6,7 @@ await prog.init()
 const desktop = await prog.makePanel({
   order: 'bottom',
   pos: [0, 0],
-  size: [prog.width, prog.height - 20],
+  size: [prog.width, prog.height - 10],
   view: (
     <view background={0x333333ff} />
   )
@@ -14,8 +14,8 @@ const desktop = await prog.makePanel({
 
 const taskbar = await prog.makePanel({
   order: 'top',
-  size: [prog.width, 20],
-  pos: [0, prog.height - 20],
+  size: [prog.width, 10],
+  pos: [0, prog.height - 10],
   view: (
     <view background={0x444444ff} />
   )
