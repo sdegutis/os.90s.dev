74e9f7df9053fa97be1f96c78ff9d986573208f4
sdegutis <sbdegutis@gmail.com>
2025-03-18 06:13:18 -0500

    Making sense...

--- site/client/views/index.ts
+++ site/client/views/index.ts
@@ -1,11 +1,30 @@
-function view() {
+import type { View } from "../../@imlib/jsx-browser.js"
+
+type WithJsx<T> = Omit<T, 'children'>
+
+interface IntrinsicView extends WithJsx<View> {
+  x: number
+  y: number
+  w: number
+  h: number
+  background: number
+  children?: JSX.Element[] | JSX.Element | undefined
+  onMouseDown?(button: number): void
+}
+
+function drawBackground(this: IntrinsicView) {
+
+}
+
+function view(): IntrinsicView {
   return {
     x: 0,
     y: 0,
     w: 0,
     h: 0,
     background: 0x00000000,
-    children: undefined as JSX.Element[] | JSX.Element | undefined,
+    children: undefined,
+    draw: drawBackground,
   }
 }
 
