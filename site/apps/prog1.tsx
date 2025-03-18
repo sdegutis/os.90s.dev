86b688a9b16d743c5f07b65c95b2cc9450b1e35c
sdegutis <sbdegutis@gmail.com>
2025-03-18 07:56:06 -0500

    About to fix clip.

--- site/apps/prog1.tsx
+++ site/apps/prog1.tsx
@@ -7,11 +7,15 @@ const panel = await prog.makePanel({
   size: [100, 100],
   view: (
     <view background={0x77000033}>
-      <border
-        borderColor={0x334455ff}
-        padding={3}
+      <view
         background={0x00770033} x={10} y={20} w={30} h={40}
-      />
+      >
+        <border
+          borderColor={0x334455ff}
+          padding={3}
+          background={0x00770033} x={10} y={20} w={30} h={40}
+        />
+      </view>
     </view>
   ),
 })
