91e9409cbb18ec9a6ce542fa5e0d3a687d1e3417
sdegutis <sbdegutis@gmail.com>
2025-03-20 13:27:19 -0500

    Cleanup.

--- site/client/views/scroll.ts
+++ site/client/views/scroll.ts
@@ -96,52 +96,6 @@ export class scroll extends view {
 
   }
 
-  // readonly area = make(view, {})
-
-  // readonly trackx = make(view, { w: 3, background: 0xffffff22, onMouseDown: () => this.dragTrack(this.trackx) })
-  // readonly tracky = make(view, { h: 3, background: 0xffffff22, onMouseDown: () => this.dragTrack(this.tracky) })
-
-  // readonly barx = make(view, { w: 3, background: 0x00000099, children: [this.trackx] })
-  // readonly bary = make(view, { h: 3, background: 0x00000099, children: [this.tracky] })
-
-  // readonly content!: view
-
-  // override init(): void {
-  //   this.addLayoutKeys('scrollx', 'scrolly')
-
-  //   this.set('content', this.children[0]!)
-  //   this.area.set('children', [this.content])
-
-  //   const mthis = this.mutable()
-  //   mthis.children = [this.area, this.barx, this.bary]
-  //   mthis.commit()
-  // }
-
-  // override onResized(): void {
-  //   this.adjustTracks()
-  // }
-
-  // override onChildResized(): void {
-  //   this.adjust?.()
-  //   this.layout()
-  // }
-
-  // private adjustTracks() {
-  //   const trackx = this.trackx.mutable()
-  //   const tracky = this.tracky.mutable()
-
-  //   const py = Math.min(1, this.h / this.content.h)
-  //   trackx.y = Math.round(this.scrolly / (this.content.h - this.h) * this.barx.h * (1 - py))
-  //   trackx.h = Math.round(this.barx.h * py)
-
-  //   const px = Math.min(1, this.w / this.content.w)
-  //   tracky.x = Math.round(this.scrollx / (this.content.w - this.w) * this.bary.w * (1 - px))
-  //   tracky.w = Math.round(this.bary.w * px)
-
-  //   trackx.commit()
-  //   tracky.commit()
-  // }
-
   // private dragTrack(track: view) {
   //   // const o = { y: this.trackx.y, x: this.tracky.x }
   //   // const drag = dragMove(o)
@@ -155,47 +109,6 @@ export class scroll extends view {
   //   //   setTimeout(() => { this.scrollVisibleClaims-- }, 500)
   //   // }
   //   // sys.trackMouse({ move, up })
-  // }
-
-  // override layout(): void {
-  //   if (!this.content) return
-
-  //   const content = this.content.mutable()
-  //   const area = this.area.mutable()
-  //   const barx = this.barx.mutable()
-  //   const bary = this.bary.mutable()
-  //   const trackx = this.trackx.mutable()
-  //   const tracky = this.tracky.mutable()
-
-  //   area.w = this.w - barx.w
-  //   area.h = this.h - bary.h
-
-  //   this.fixScrollPos()
-  //   content.x = -this.scrollx
-  //   content.y = -this.scrolly
-
-  //   barx.x = this.w - barx.w
-  //   barx.y = 0
-  //   barx.h = this.h - bary.h
-
-  //   bary.y = this.h - bary.h
-  //   bary.x = 0
-  //   bary.w = this.w - barx.w
-
-  //   trackx.x = 0
-  //   trackx.w = barx.w
-
-  //   tracky.y = 0
-  //   tracky.h = bary.h
-
-  //   content.commit()
-  //   area.commit()
-  //   barx.commit()
-  //   bary.commit()
-  //   trackx.commit()
-  //   tracky.commit()
-
-  //   this.adjustTracks()
   // }
 
 }
