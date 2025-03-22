d06908dc85710d6ab323910bee54e90ffd67e9a2
sdegutis <sbdegutis@gmail.com>
2025-03-22 05:38:21 -0500

    Order.

--- site/main.ts
+++ site/main.ts
@@ -3,8 +3,11 @@ import { Sys } from './server/sys.js'
 
 const sys = new Sys(320, 180)
 
-const shell = new Process(sys, '/apps/shell.js')
+const shell = new Process(sys, '/apps/shell.js'); await sleep(.01)
+const proc2 = new Process(sys, '/apps/prog2.js'); await sleep(.01)
+const proc3 = new Process(sys, '/apps/prog3.js'); await sleep(.01)
+const proc1 = new Process(sys, '/apps/prog1.js'); await sleep(.01)
 
-const proc1 = new Process(sys, '/apps/prog1.js')
-const proc2 = new Process(sys, '/apps/prog2.js')
-const proc3 = new Process(sys, '/apps/prog3.js')
+function sleep(s: number) {
+  return new Promise(r => setTimeout(r, s * 1000))
+}
