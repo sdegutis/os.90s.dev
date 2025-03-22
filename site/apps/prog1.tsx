da7dca597116f978b014960d6c3d7a5dde33c1a9
sdegutis <sbdegutis@gmail.com>
2025-03-22 16:42:18 -0500

    Cleanup.

--- site/apps/prog1.tsx
+++ site/apps/prog1.tsx
@@ -1,49 +1,18 @@
 import { program } from "../client/core/prog.js"
 import { PanelView } from "../client/util/panelview.js"
-import { showPrompt } from "../client/util/prompt.js"
-import { $, multiplex } from "../client/util/ref.js"
+import { $ } from "../client/util/ref.js"
 
 const size = $({ w: 100, h: 100 })
-
-const textarea = <textarea text={''} background={0x99000099} multiline={false} onEnter={function () {
-  console.log('hey', this.text)
-}} />
-
-// const textarea = <textarea />
-
-// const scroll = <scroll
-//   background={0xffffff11}
-//   onMouseDown={function (...args) { this.content?.onMouseDown?.(...args) }}
-// >
-//   {textarea}
-// </scroll>
 
 const panel = await program.makePanel({
   size,
   view: <PanelView size={size} title={'test panel'}>
-    <panedyb>
-      <scroll
-        // showv={false}
-        // showh={false}
-        adoptedByParent={function (p) {
-          multiplex([textarea.$ref('size'), p.$ref('size')], () => {
-            this.point = { x: 5, y: 0 }
-            this.size = { w: p.size.w - 10, h: textarea.size.h }
-          })
-        }}
-        background={0xffffff11}
-        onMouseDown={function (...args) { this.firstChild?.onMouseDown?.(...args) }}
-      >
-        {textarea}
-      </scroll>
-      <groupx>
-        <button onClick={async () => {
-          console.log(await showPrompt('hey? hows it going?\nwell this is fine'))
-        }} padding={3}><label text={'hmm'} /></button>
-      </groupx>
-    </panedyb>
+    <scroll background={0xffffff11} onMouseDown={function (...args) { this.content?.onMouseDown?.(...args) }}>
+      <textarea adoptedByPanel={function () {
+        this.focus()
+      }} />
+    </scroll>
   </PanelView>,
 })
 
-textarea.focus()
 panel.focus()
