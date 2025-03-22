83eec5366259f612703496260f76398e1ed9650a
sdegutis <sbdegutis@gmail.com>
2025-03-22 14:32:08 -0500

    Function.

--- site/apps/prog1.tsx
+++ site/apps/prog1.tsx
@@ -4,40 +4,48 @@ import { dragMove } from "../client/util/drag.js"
 const prog = new Program()
 await prog.init()
 
-const dialog = <border
-  passthrough={false}
-  onPanelBlur={no}
-  canFocus={true}
-  onKeyDown={key => {
-    if (key === 'Enter') ok()
-    if (key === 'Escape') no()
-  }}
-  onMouseDown={function (b, pos) {
-    this.onMouseMove = dragMove(pos, panel)
-    this.onMouseUp = () => delete this.onMouseMove
-  }}
-  background={0x000000cc} padding={1} borderColor={0x005599ff}>
-  <border padding={3}>
-    <groupy align={'z'} gap={4}>
-      <border padding={2}>
-        <label text={'are you sure?'} />
-      </border>
-      <groupx gap={2}>
-        <button onClick={no} background={0x99000099} padding={2}><label text={'cancel'} /></button>
-        <button onClick={ok} background={0xffffff33} padding={2}><label text={'ok'} /></button>
-      </groupx>
-    </groupy>
+async function showDialog(prog: Program, text: string) {
+  const result = Promise.withResolvers()
+
+  const dialog = <border
+    passthrough={false}
+    onPanelBlur={no}
+    canFocus={true}
+    onKeyDown={key => {
+      if (key === 'Enter') ok()
+      if (key === 'Escape') no()
+    }}
+    onMouseDown={function (b, pos) {
+      this.onMouseMove = dragMove(pos, panel)
+      this.onMouseUp = () => delete this.onMouseMove
+    }}
+    background={0x000000cc} padding={1} borderColor={0x005599ff}>
+    <border padding={3}>
+      <groupy align={'z'} gap={4}>
+        <border padding={2}>
+          <label text={text} />
+        </border>
+        <groupx gap={2}>
+          <button onClick={no} background={0x99000099} padding={2}><label text={'cancel'} /></button>
+          <button onClick={ok} background={0xffffff33} padding={2}><label text={'ok'} /></button>
+        </groupx>
+      </groupy>
+    </border>
   </border>
-</border>
 
-const panel = await prog.makePanel({
-  size: dialog.$ref('size'),
-  pos: 'center',
-  view: dialog,
-})
+  const panel = await prog.makePanel({
+    size: dialog.$ref('size'),
+    pos: 'center',
+    view: dialog,
+  })
 
-panel.focus()
-dialog.focus()
+  panel.focus()
+  dialog.focus()
 
-function ok() { console.log('ok'); panel.close() }
-function no() { console.log('no'); panel.close() }
+  function ok() { panel.close(); result.resolve(true) }
+  function no() { panel.close(); result.resolve(false) }
+
+  return result.promise
+}
+
+console.log(await showDialog(prog, 'hey? hows it going?\nwell this is fine'))
