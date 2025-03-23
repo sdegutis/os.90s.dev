a04753a5f661a8762be71857f95f6c51f3018bad
sdegutis <sbdegutis@gmail.com>
2025-03-23 15:36:59 -0500

    Done for now.

--- site/apps/prog1.tsx
+++ site/apps/prog1.tsx
@@ -8,18 +8,11 @@ const panel = await program.makePanel({
   size,
   pos: 'center',
   view: <PanelView size={size} title={'test panel'}>
-    <splitxa>
-      <scroll background={0xffffff11} onMouseDown={function (...args) { this.content?.onMouseDown?.(...args) }}>
-        <textarea presented={function () {
-          this.focus()
-        }} />
-      </scroll>
-      <margin padding={1} marginColor={0x000099ff}>
-        <center background={0x99000033}>
-          <button background={0x00990033} padding={2}><label text={'hmm'} /></button>
-        </center>
-      </margin>
-    </splitxa>
+    <scroll background={0xffffff11} onMouseDown={function (...args) { this.content?.onMouseDown?.(...args) }}>
+      <textarea presented={function () {
+        this.focus()
+      }} />
+    </scroll>
   </PanelView>,
 })
 
