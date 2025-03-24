a4dd6d60354ba2cf5d98b9dbd10bbefefb86233f
sdegutis <sbdegutis@gmail.com>
2025-03-24 17:28:57 -0500

    For now.

--- site/apps/prog1.tsx
+++ site/apps/prog1.tsx
@@ -1,44 +1,12 @@
 import { Panel } from "../client/core/panel.js"
-import { showDialog } from "../client/util/dialog.js"
 import { PanelView } from "../client/util/panelview.js"
-import { showPrompt } from "../client/util/prompt.js"
-import type { Split } from "../client/views/split.js"
 import { $ } from "../shared/ref.js"
 
 
-const t = `
-fasdfasdfsadf
-fasdfasdfsadf
-fasdfasdfsadf
-fasdfasdfsadf
-fasdfasdfsadf
-`
-
 const panel = await Panel.create(
   <PanelView size={$({ w: 75, h: 50 })} title={'test panel'}>
-    <splitxa>
-      <margin padding={0} paddingColor={0x99000099}>
-        <center background={0x00330099}>
-          <button onClick={async function () {
-            const s = this.parent?.parent?.parent as Split
-            s.dir = s.dir === 'x' ? 'y' : 'x'
-            s.stick = s.stick === 'a' ? 'b' : 'a'
-
-            await showDialog('hey1')
-            await showPrompt('hey2 much longer string')
-
-          }} padding={2}><label text={'test'} /></button>
-        </center>
-      </margin>
-      <scroll background={0xffffff11} onMouseDown={function (...args) { this.content?.onMouseDown?.(...args) }}>
-        <textarea autofocus text={t} />
-      </scroll>
-    </splitxa>
+    <view />
   </PanelView>
 )
 
 panel.focusPanel()
-
-// setTimeout(() => {
-//   program.resize(320 * 2, 180 * 2)
-// }, 1000)
