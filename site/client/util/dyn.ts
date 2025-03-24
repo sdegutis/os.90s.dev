d85ac7b0b43bc213fb43f654e56de0b0ac77a42e
sdegutis <sbdegutis@gmail.com>
2025-03-24 13:20:23 -0500

    Inline.

--- site/client/util/dyn.ts
+++ site/client/util/dyn.ts
@@ -4,30 +4,20 @@ export class Dynamic {
 
   static make<T extends Dynamic>(this: new () => T, data: { [K in keyof T]?: T[K] | Ref<T[K]> }) {
     const v = new this()
+
     const init = data.init
     delete data.init
+
     Object.assign(v, data)
-    v.#setup()
-    const initfn = (init instanceof Ref ? init.val : init)
-    initfn?.apply(v)
-    return v
-  }
 
-  init?(): void
-
-  $ = Object.create(null) as {
-    readonly [K in (keyof this & string) as (K extends '$' ? never : K)]: this[K] extends ((...args: any) => any) | undefined ? never : Ref<this[K]>
-  }
-
-  #setup() {
-    for (const key in this) {
-      let val = this[key]
+    for (const key in v) {
+      let val = v[key]
       if (val instanceof Function) continue
 
       const ref = val instanceof Ref ? val : $(val);
-      (this.$ as any)[key] = ref
+      (v.$ as any)[key] = ref
 
-      Object.defineProperty(this, key, {
+      Object.defineProperty(v, key, {
         get: () => ref.val,
         set: (v) => ref.val = v,
         enumerable: true,
@@ -35,14 +25,25 @@ export class Dynamic {
     }
 
     const protos = []
-    let proto: Dynamic | undefined = this
+    let proto: Dynamic | undefined = v
 
     while (proto = Object.getPrototypeOf(proto))
       if (Object.hasOwn(proto, 'init'))
         protos.push(proto)
 
     while (proto = protos.pop())
-      proto.init!.call(this)
+      proto.init!.call(v)
+
+    const initfn = (init instanceof Ref ? init.val : init)
+    initfn?.apply(v)
+
+    return v
+  }
+
+  init?(): void
+
+  $ = Object.create(null) as {
+    readonly [K in (keyof this & string) as (K extends '$' ? never : K)]: this[K] extends ((...args: any) => any) | undefined ? never : Ref<this[K]>
   }
 
 }
