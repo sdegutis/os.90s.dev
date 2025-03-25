18eb47b6a5b452634d054c8ccb9aae7aff9a87ca
sdegutis <sbdegutis@gmail.com>
2025-03-24 21:54:02 -0500

    Cool.

---
+++ site/apps/imports.d.ts
@@ -0,0 +1,5 @@
+declare module 'https://esm.sh/@swc/wasm-web' {
+  import initSwc from '@swc/wasm-web'
+  export * from '@swc/wasm-web'
+  export default initSwc
+}
---
+++ site/client/util/compile.ts
@@ -0,0 +1,20 @@
+import initSwc, { transformSync } from 'https://esm.sh/@swc/wasm-web'
+
+await initSwc()
+
+export function testCompile() {
+
+  console.log('hey', transformSync('const a:number = <foo>33</foo>', {
+    isModule: true,
+    jsc: {
+      parser: { syntax: 'typescript', tsx: true },
+      transform: {
+        react: {
+          runtime: 'automatic',
+          importSource: '/foo',
+        },
+      }
+    }
+  }).code)
+
+}
