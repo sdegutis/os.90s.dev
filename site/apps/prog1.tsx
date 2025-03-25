21bf1ea154bf63e30389632abba4fa728371c65b
sdegutis <sbdegutis@gmail.com>
2025-03-25 15:43:25 -0500

    Hmm.

--- site/apps/prog1.tsx
+++ site/apps/prog1.tsx
@@ -2,10 +2,17 @@ import { Panel } from "../client/core/panel.js"
 import { PanelView } from "../client/util/panelview.js"
 import { $ } from "../shared/ref.js"
 
+const TEST = `
+this is a long string
+it has multiple lines
+hmm
+it is not colorful yet...
+`.trimStart()
+
 const panel = await Panel.create(
   <PanelView title={'Writer'} size={$({ w: 100, h: 70 })}>
     <scroll background={0xffffff11} onMouseDown={function (b) { this.content.onMouseDown?.(b) }}>
-      <textarea autofocus />
+      <label text={TEST} />
     </scroll>
   </PanelView>
 )
