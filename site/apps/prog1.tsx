c53ce10903fdc9be2dddad57950527c6ff2cd347
sdegutis <sbdegutis@gmail.com>
2025-03-26 07:42:01 -0500

    Test.

--- site/apps/prog1.tsx
+++ site/apps/prog1.tsx
@@ -11,11 +11,14 @@ it is not colorful yet...
 
 const panel = await Panel.create(
   <PanelView title={'Writer'} size={$({ w: 100, h: 70 })}>
-    <scroll background={0xffffff11} onMouseDown={function (b) { this.content.onMouseDown?.(b) }}>
-      <border padding={2}>
-        <label text={TEST} />
-      </border>
-    </scroll>
+    <splitya>
+      <button><label text={'hmm'} /></button>
+      <scroll background={0xffffff11} onMouseDown={function (b) { this.content.onMouseDown?.(b) }}>
+        <border padding={2}>
+          <label text={TEST} />
+        </border>
+      </scroll>
+    </splitya>
   </PanelView>
 )
 
