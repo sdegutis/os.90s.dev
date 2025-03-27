1cfacfc7abab609d8f0fbf4d70d08bbfc9345eeb
sdegutis <sbdegutis@gmail.com>
2025-03-26 20:15:26 -0500

    inline

--- site/shared/cursor.ts
+++ site/shared/cursor.ts
@@ -1,5 +1,4 @@
 import { Bitmap } from "/shared/bitmap.js"
-import { DataFile } from "/shared/datafile.js"
 
 export class Cursor {
 
@@ -30,6 +29,44 @@ export class Cursor {
 
   draw(ctx: CanvasRenderingContext2D | OffscreenCanvasRenderingContext2D, px: number, py: number) {
     ctx.drawImage(this.bitmap.canvas, px - this.offx, py - this.offy)
+  }
+
+}
+
+class DataFile<T> {
+
+  meta: T | null
+  data: string
+
+  constructor(contents: string) {
+    const [meta, data] = this.parseMeta(contents)
+    this.meta = meta
+    this.data = data
+  }
+
+  private parseMeta(str: string): [T | null, string] {
+    const split = str.match(/\n===+\n/)
+    if (!split || split.index === undefined) return [null, str]
+
+    const head = str.slice(0, split.index).trim()
+    const rest = str.slice(split.index + split[0].length)
+    const meta = Object.create(null)
+
+    for (const line of head.split('\n')) {
+      const [k, v] = (line.split(/ *= */))
+
+      const parts = k.split('.')
+      const last = parts.pop()!
+
+      let node = meta
+      for (const p of parts) {
+        node = node[p] ??= Object.create(null)
+      }
+
+      node[last] = v
+    }
+
+    return [meta, rest]
   }
 
 }
