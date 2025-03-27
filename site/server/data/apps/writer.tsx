0e5e21079164c2311e20e14676ec7999c5f5fb4e
sdegutis <sbdegutis@gmail.com>
2025-03-27 09:35:09 -0500

    Writer broken.

--- site/server/data/apps/writer.tsx
+++ site/server/data/apps/writer.tsx
@@ -1,32 +1,23 @@
 import { Panel } from "/client/core/panel.js"
 import { $ } from "/client/core/ref.js"
 import { program, sys } from "/client/core/sys.js"
-import { PanelView } from "/client/util/panelview.js"
+import { FilePanelView } from "/client/util/panelview.js"
 import type { Textarea } from "/client/views/textarea.js"
 
 const $content = $('')
-
-let filepath = program.opts["file"]
-if (filepath) $content.val = await sys.getfile(filepath) ?? ''
+const $filepath = $(program.opts["file"])
+if ($filepath.val) $content.val = await sys.getfile($filepath.val) ?? ''
 
 const textarea = <textarea text={$content.val} autofocus /> as Textarea
 
 const panel = await Panel.create(
-  <PanelView title={$('writer')} size={$({ w: 100, h: 70 })}>
-    <scroll onKeyDown={handleKey} background={0xffffff11} onMouseDown={function (b) { this.content.onMouseDown?.(b) }}>
+  <FilePanelView filedata={$content} filepath={$filepath} title={$('writer')} size={$({ w: 100, h: 70 })}>
+    <scroll background={0xffffff11} onMouseDown={function (b) { this.content.onMouseDown?.(b) }}>
       <border canMouse padding={2} onMouseDown={function (b) { this.firstChild!.onMouseDown?.(b) }}>
         {textarea}
       </border>
     </scroll>
-  </PanelView>
+  </FilePanelView>
 )
 
 panel.focusPanel()
-
-function handleKey(key: string) {
-  if (key === 's' && panel.isKeyDown('Control')) {
-    sys.putfile(filepath, textarea.text)
-    return true
-  }
-  return false
-}
