63c1f3adaa06a9e990f31ebfd268b5f1b2d25631
sdegutis <sbdegutis@gmail.com>
2025-03-26 21:29:01 -0500

    So clean.

--- site/server/data/apps/filer.tsx
+++ site/server/data/apps/filer.tsx
@@ -9,29 +9,38 @@ import type { View } from "/client/views/view.js"
 const drives = await program.listdrives('')
 
 const $entries = $<View[]>([])
+const $breadcrumbs = $<View[]>([])
 
 const panel = await Panel.create(
   <PanelView title={'Filer'} size={$({ w: 150, h: 120 })}>
     <splitxa pos={50}>
       <view>
         <groupy align={'+'} children={drives.map(d =>
-          <button padding={2} onClick={() => showDir([], d)}>
+          <button padding={2} onClick={() => showDir([d])}>
             <label text={d} />
           </button>
         )} />
       </view>
-      <scroll background={0xffffff11} onMouseDown={function (b) { this.content.onMouseDown?.(b) }}>
-        <groupy align={'+'} children={$entries} />
-      </scroll>
+      <panedya>
+        <groupx children={$breadcrumbs} />
+        <scroll background={0xffffff11} onMouseDown={function (b) { this.content.onMouseDown?.(b) }}>
+          <groupy align={'+'} children={$entries} />
+        </scroll>
+      </panedya>
     </splitxa>
   </PanelView>
 )
 
 panel.focusPanel()
 
-async function showDir(base: string[], dir: string) {
-  const full = [...base, dir]
+async function showDir(full: string[]) {
   const results = await program.listdir(full.join(''))
+
+  $breadcrumbs.val = full.map((part, idx) =>
+    <button padding={2} onClick={() => showDir(full.slice(0, idx + 1))}>
+      <label text={part} />
+    </button>
+  )
 
   if (results.length === 0) {
     $entries.val = [
@@ -54,7 +63,7 @@ const imgFile = new Bitmap([0x009900ff], 1, [1])
 
 function FolderItem({ base, item }: { base: string[], item: FolderItem }) {
   return (
-    <button padding={2} onClick={() => showDir(base, item.name)}>
+    <button padding={2} onClick={() => showDir([...base, item.name])}>
       <groupx gap={2}>
         <border>
           <image bitmap={imgFolder} />
