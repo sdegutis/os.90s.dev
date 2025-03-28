2636df59dc6c37e3cc4817c0c72930c8cf5c7dc2
sdegutis <sbdegutis@gmail.com>
2025-03-28 07:21:21 -0500

    No need now.

--- site/swc/vm.ts
+++ site/swc/vm.ts
@@ -2,17 +2,11 @@ import initSwc, { transformSync } from "/swc/wasm.js"
 
 await initSwc()
 
-export async function exec(tsx: string) {
-  const compiled = compile(tsx)
-  const fn = new Function('System', compiled)
-  fn(sysjs)
-}
-
-function compile(tsx: string) {
+export function compile(tsx: string) {
   return transformSync(tsx, {
     isModule: true,
     module: {
-      type: 'systemjs'
+      type: 'es6'
     },
     jsc: {
       parser: { syntax: 'typescript', tsx: true },
@@ -24,21 +18,4 @@ function compile(tsx: string) {
       }
     }
   }).code
-}
-
-const sysjs = {
-  register: async (deps: string[], fn: (exp: (k: string, v: any) => void, ctx: any) => {
-    setters: ((dep: any) => void)[],
-    execute: () => Promise<any>,
-  }) => {
-    deps = deps.map(d => d === '/@imlib/jsx-runtime' ? '/jsx.js' : d)
-    const imps = await Promise.all(deps.map(dep => import(dep)))
-    const { setters, execute } = fn((k, v) => {
-
-    }, {})
-    for (let i = 0; i < imps.length; i++) {
-      setters[i](imps[i])
-    }
-    await execute()
-  }
 }
