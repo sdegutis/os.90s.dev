3477c9e326c6afdd90c0015c6782e39f2ecf03b5
sdegutis <sbdegutis@gmail.com>
2025-03-28 15:24:23 -0500

    Terminal?

---
+++ site/fs/sys/apps/terminal.tsx
@@ -0,0 +1,34 @@
+const $lines = $<View[]>([])
+const textarea = <Textarea autofocus multiline={false} onEnter={submit} /> as Textarea
+const context = {}
+
+function append(view: View) {
+  $lines.val = [...$lines.val, view]
+}
+
+function submit() {
+  let toeval = textarea.text
+  textarea.text = ''
+
+  append(<Label text={toeval} />)
+
+  if (!toeval.match(/^(const|var|let)/)) toeval = `return ${toeval}`
+  try {
+    const fn = new Function(toeval)
+    const res = fn.apply(context)
+    append(<Label text={'> ' + res} />)
+  }
+  catch (e: any) {
+    append(<Label text={e.toString()} textColor={0x99000099} />)
+  }
+}
+
+const panel = await Panel.create(
+  <PanelView title={$('terminal')} size={$({ w: 100, h: 70 })}>
+    <Scroll background={0xffffff11} onMouseDown={function (b) { this.content.onMouseDown?.(b) }}>
+      <GroupY align={'a'} children={$lines.adapt(ls => [...ls, textarea])} />
+    </Scroll>
+  </PanelView>
+)
+
+panel.focusPanel()
