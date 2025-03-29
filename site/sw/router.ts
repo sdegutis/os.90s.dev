06f33e4511507ff067e85574bcd116e1cfa88c32
sdegutis <sbdegutis@gmail.com>
2025-03-28 23:36:48 -0500

    Fix.

--- site/sw/router.ts
+++ site/sw/router.ts
@@ -1,8 +1,6 @@
 import { openUserDb } from "../server/fs/userfs.js"
 import { compile } from "./compile.js"
 
-const fsinit = openUserDb()
-
 const jsHeaders = {
   headers: {
     "content-type": "application/javascript; charset=utf-8"
@@ -15,8 +13,6 @@ async function jsResponse(text: string) {
 }
 
 export async function handleRoute(url: URL, req: Request) {
-  const fs = await fsinit
-
   if (url.pathname.startsWith('/fs/sys/')) {
     if (url.pathname.endsWith('.js')) {
       let text = await fetch(req).then(r => r.text())
@@ -27,6 +23,7 @@ export async function handleRoute(url: URL, req: Request) {
 
   if (url.pathname.startsWith('/fs/user/')) {
     const key = url.pathname.slice('/fs/user/'.length)
+    const fs = await openUserDb()
     const res = await fs.get(key)
     return await jsResponse(res?.content ?? '')
   }
