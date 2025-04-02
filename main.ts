91e07817e8e10b817e06885e290cc0e63ff56189
sdegutis <sbdegutis@gmail.com>
2025-04-02 08:29:30 -0500

    Upgrade.

--- main.ts
+++ main.ts
@@ -6,100 +6,70 @@ const swc2 = fs.readFileSync('node_modules/@swc/wasm-web/wasm_bg.wasm')
 
 const tree = new immaculata.LiveTree('site', import.meta.url)
 
-const compile = immaculata.makeSwcTransformJsx(() => '/client/jsx.js')
-
-const ext = (s: string) => s.match(/\.([^\/]+)$/)?.[1] ?? ''
-
 const copyright = `Copyright ©️ ${new Date().getFullYear()} Novo Cantico LLC. All rights reserved.`
 
-const icon = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 5 5">
-  <path d="M1 0 L3 2 1 4 Z" fill="#19f" />
-</svg>`
+const icon = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 5 5"><path d="M1 0 L3 2 1 4 Z" fill="#19f" /></svg>`
+const iconlink = `<link rel="shortcut icon" href="${`data:image/svg+xml,${encodeURIComponent(icon)}`}" />`
 
 function processSite() {
   return tree.processFiles(files => {
 
-    files = files.filter(f => !f.path.endsWith('.d.ts'))
+    files.with(/\.d\.ts$/).remove()
 
-    files = files.map(file => {
-      if (!file.path.match(/\.tsx?$/)) return file
-
-      const path = file.path.replace(/\.tsx?$/, '.js')
-
-      let content = file.content.toString()
-      content = `// ${copyright}\n\n` + content
-
-      if (file.path.startsWith('/fs/sys/')) {
-        return { content, path }
-      }
-
-      content = compile('', file.path, content.toString(), true)
-      return { content, path }
+    files.with(/\.tsx?$/).without('^/fs/sys/').do(file => {
+      file.text = immaculata.compileWithSwc(file.text, opts => {
+        opts.filename = file.path
+        opts.jsc ??= {}
+        opts.jsc.transform ??= {}
+        opts.jsc.transform.react ??= {}
+        opts.jsc.transform.react.importSource = '/client/jsx.js'
+      }).code
     })
 
-    files.push({ path: '/sw/wasm.js', content: swc1 })
-    files.push({ path: '/sw/wasm_bg.wasm', content: swc2 })
+    files.with(/\.tsx?$/).do(file => { file.path = file.path.replace(/\.tsx?$/, '.js') })
 
-    const exports = files
-      .filter(f => f.path.startsWith('/client/'))
-      .map(f => `export * from ".${f.path}"`)
-      .join('\n')
+    files.with(/\.js$/).do(file => { file.text = `// ${copyright}\n\n` + file.text })
+    files.with(/\.html$/).do(file => { file.text = `<!-- ${copyright} -->\n\n` + file.text })
 
+    files.add('/sw/wasm.js', swc1)
+    files.add('/sw/wasm_bg.wasm', swc2)
+
+    const exports = files.with('^/client/').all().map(f => `export * from ".${f.path}"`).join('\n')
     fs.writeFileSync('./site/api.d.ts', exports)
-    files.push({ path: '/api.js', content: exports })
+    files.add('/api.js', exports)
 
     const sysdata = JSON.stringify(Object.fromEntries(files
-      .filter(f => f.path.startsWith('/fs/sys'))
-      .map(f => [f.path.slice('/fs/sys/'.length), f.content.toString()])
+      .with('^/fs/sys')
+      .all().map(f => [f.path.slice('/fs/sys/'.length), f.text])
     ), null, 2)
-    files.push({ path: '/server/fs/data.js', content: `export const files = ${sysdata}` })
+    files.add('/server/fs/data.js', `export const files = ${sysdata}`)
 
-    const paths = files.map(f => f.path).filter(s => !s.startsWith('/fs/sys'))
+    const paths = files.without('^/fs/sys')
 
-    const datas = (paths
-      .filter(s => !['js', 'html', 'wasm'].includes(ext(s)))
-      .map(s => `<link rel="preload" as="fetch" href="${s.replace(/\.js$/, '')}" crossorigin="anonymous" />`))
+    const datas = paths.without('\.js$|\.html$|\.wasm$').paths()
+      .map(path => `<link rel="preload" as="fetch" href="${path}" crossorigin="anonymous" />`)
 
-    const modules = (paths
-      .filter(s => ext(s) === 'js')
-      .map(s => `<link rel="modulepreload" href="${s}" />`))
+    const modules = paths.with('\.js$').paths()
+      .map(path => `<link rel="modulepreload" href="${path}" />`)
 
-    const iconlink = `<link rel="shortcut icon" href="${`data:image/svg+xml,${encodeURIComponent(icon)}`}" />`
-    let hmr = ``
-    // hmr = `<script>new EventSource('/@imlib/hmr').onmessage = () => location.reload()</script>`
-    const headers = [...datas, ...modules, iconlink, hmr]
-    const toinsert = headers.map(s => `  ${s}`).join('\n')
+    const toinsert = [...datas, ...modules, iconlink].map(s => `  ${s}`).join('\n')
+    files.with(/\.html$/).do(file => file.text = file.text.replace('<head>', `<head>\n${toinsert}`))
 
-    function insert(s: string) {
-      return s.replace('<head>', `<head>\n${toinsert}`)
-    }
-
-    files = files.map(file => {
-      if (file.path.endsWith('.html')) {
-        return {
-          path: file.path,
-          content: `<!-- ${copyright} -->\n\n` + insert(file.content.toString()),
-        }
-      }
-      return file
-    })
-
-    return files
   })
 }
 
 if (process.argv[2] === 'dev') {
   const server = new immaculata.DevServer(8080)
-  server.files = processSite()
+  server.files = await processSite()
 
   tree.watch({
     ignored: (str) => str.endsWith('/site/api.d.ts')
-  }, (paths) => {
+  }, async (paths) => {
     console.log('paths changed')
-    server.files = processSite()
+    server.files = await processSite()
     server.reload()
   })
 }
 else {
-  immaculata.generateFiles(processSite())
+  immaculata.generateFiles(await processSite())
 }
