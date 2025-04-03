cf22bac54de18321873bfd8245e3c7cf37d15bb0
sdegutis <sbdegutis@gmail.com>
2025-04-03 12:41:43 -0500

    Not ready.

--- site/fs/sys/apps/writer.tsx
+++ site/fs/sys/apps/writer.tsx
@@ -9,9 +9,7 @@ const textarea = <api.Textarea text={content} autofocus /> as api.Textarea
 const panel = await api.Panel.create({ name: "writer" },
   <api.FilePanelView filedata={() => textarea.text} filepath={$filepath} title={api.$('writer')} size={api.$({ w: 100, h: 70 })}>
     <api.Scroll background={0xffffff11} onMouseDown={function (b) { this.content.onMouseDown?.(b) }}>
-      <api.Border canMouse padding={2} onMouseDown={function (b) { this.firstChild!.onMouseDown?.(b) }}>
-        {textarea}
-      </api.Border>
+      {textarea}
     </api.Scroll>
   </api.FilePanelView>
 )
