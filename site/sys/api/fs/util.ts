9233b6208068afab66f124cc1c3a4b90f1da6d9d
sdegutis <sbdegutis@gmail.com>
2025-04-04 15:10:30 -0500

    Fixes.

--- site/sys/api/fs/util.ts
+++ site/sys/api/fs/util.ts
@@ -1,8 +1,9 @@
 export function listdir(full: string, entries: { path: string, content?: string }[]): string[] {
   return entries
-    .map(file => file.path)
+    .map(file => file.content === undefined ? file.path + '/' : file.path)
     .filter(path => path.startsWith(full))
     .map(path => path.slice(full.length))
+    .filter(name => name)
     .map(name => name.match(/^[^\/]+\/?/)![0])
     .reduce((set, name) => set.add(name), new Set<string>())
     .values()
