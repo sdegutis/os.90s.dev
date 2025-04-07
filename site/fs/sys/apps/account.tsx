ebd0aa898a7a2c787efb127123a9f00b21ce1aaa
sdegutis <sbdegutis@gmail.com>
2025-04-07 07:43:27 -0500

    Learning auth.

--- site/fs/sys/apps/account.tsx
+++ site/fs/sys/apps/account.tsx
@@ -1,44 +1,51 @@
 import * as api from '/api.js'
 
 
-// const mylocalstorage = await api.opendb<{ key: string, val: any }>('mylocalstorage', 'key')
+const userinfo = await api.opendb<{ key: string, val: any }>('userinfo', 'key')
 
 
-
-// const user = await mylocalstorage.get('username')
-
+// const user = await userinfo.get('username')
 // if (!user) {
 
-//   console.log(await fetch(api.config.net + '/newuser', {
-//     method: 'post',
-//     body: 'theadmin3 admin@90s.dev yes',
-//     credentials: 'include',
-//   }).then(r => {
-//     console.log(r.status)
+console.log(await fetch(api.config.net + '/newuser', {
+  method: 'post',
+  body: 'theadmin3 admin@90s.dev',
+  credentials: 'include',
+}).then(r => {
+  console.log(...r.headers)
+  console.log(r.status)
 
-//     if (r.status === 200) {
-//       mylocalstorage.set({ key: 'username', val: 'theadmin' })
-//     }
+  if (r.status === 200) {
+    userinfo.set({ key: 'username', val: 'theadmin3' })
+  }
 
-//     return r.text()
-//   }))
+  return r.text()
+}))
+
 // }
 // else {
 
 //   console.log(await fetch(api.config.net + '/fs/', {
-//     credentials: 'include',
+
 //   }).then(r => {
+
+
+
+//     console.log(...r.headers)
 //     console.log(r.status)
 //     return r.text()
 //   }))
 
 // }
 
+async function create() {
 
-const panel = await api.Panel.create({ name: 'filer' },
-  <api.PanelView title={api.$('filer')} size={api.$({ w: 150, h: 120 })}>
+}
+
+const panel = await api.Panel.create({ name: 'account' },
+  <api.PanelView title={api.$('account')} size={api.$({ w: 150, h: 120 })}>
     <api.Center>
-      <api.Button padding={2}>
+      <api.Button padding={2} onClick={create}>
         <api.Label text='create account' />
       </api.Button>
     </api.Center>
