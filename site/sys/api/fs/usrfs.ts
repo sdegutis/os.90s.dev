e5fe939d1c5879ad6e094994a4d8b0afefe22ab9
sdegutis <sbdegutis@gmail.com>
2025-04-09 19:41:58 -0500

    Fixed.

--- site/sys/api/fs/usrfs.ts
+++ site/sys/api/fs/usrfs.ts
@@ -1,72 +1,55 @@
 import { opendb } from "../util/db.js"
 import type { Drive } from "./drive.js"
 
-
-
-function listdir(full: string, entries: { path: string, content?: string }[]): string[] {
-  return entries
-    .map(file => file.content === undefined ? file.path + '/' : file.path)
-    .filter(path => path.startsWith(full))
-    .map(path => path.slice(full.length))
-    .filter(name => name)
-    .map(name => name.match(/^[^\/]+\/?/)![0])
-    .reduce((set, name) => set.add(name), new Set<string>())
-    .values()
-    .toArray()
-}
-
-
-// new EventSource('/_reload').onmessage = () => location.reload()
-
-// navigator.locks.request('testing', async () => {
-
-//   console.log(files)
-//   console.log(listdir(['default', 'conf', ''].join('/'), files))
-
-//   await new Promise(r => { })
-// })
-
-
-const usrdb = opendb<{ path: string, content?: string }>('usr', 'path')
+const db = await opendb<{
+  path: string,
+  file: string,
+  dir: string,
+  content?: string,
+}>('usr', 'path')
 
 export class UsrDrive implements Drive {
 
   async getDir(path: string[]): Promise<string[]> {
-    const db = await usrdb
     const all = await db.all()
-    return listdir(path.join('/'), all)
+    const parent = path.join('/')
+    return all.filter(it => it.dir === parent).map(it => it.file)
   }
 
   async putDir(path: string[]): Promise<boolean> {
-    const db = await usrdb
-    await db.set({ path: path.join('/') })
+    let last = ''
+    for (let dir of path.slice(0, -1)) {
+      dir += '/'
+      await db.set({ dir: last, file: dir, path: last + dir })
+      last += dir
+    }
     return true
   }
 
   async delDir(path: string[]): Promise<boolean> {
-    const db = await usrdb
     const all = await db.all()
     const full = path.join('/')
-    const toDelete = all.map(f => f.path).filter(p => p.startsWith(full))
-    toDelete.push(full.slice(0, -1))
-    await Promise.all(toDelete.map(p => db.del(p)))
+    const within = all.filter(it => it.dir.startsWith(full))
+    await Promise.all([
+      db.del(full),
+      ...within.map(it => db.del(it.path))
+    ])
     return true
   }
 
   async getFile(path: string[]): Promise<string | null> {
-    const db = await usrdb
     const file = await db.get(path.join('/'))
     return file?.content ?? null
   }
 
   async putFile(path: string[], content: string): Promise<boolean> {
-    const db = await usrdb
-    await db.set({ path: path.join('/'), content })
+    const file = path.pop()!
+    const dir = path.map(d => d + '/').join('')
+    await db.set({ dir, file, content, path: dir + file })
     return true
   }
 
   async delFile(path: string[]): Promise<boolean> {
-    const db = await usrdb
     await db.del(path.join('/'))
     return true
   }
--- site/sys/api/util/db.ts
+++ site/sys/api/util/db.ts
@@ -1,7 +1,7 @@
-export async function opendb<T>(dbname: string, key: keyof T & string) {
+export async function opendb<T>(dbname: string, keyPath: keyof T & string) {
   const db = await new Promise<IDBDatabase>(resolve => {
     const r = self.indexedDB.open(dbname, 1)
-    r.onupgradeneeded = () => { r.result.createObjectStore('kvs', { keyPath: key }) }
+    r.onupgradeneeded = () => { r.result.createObjectStore('kvs', { keyPath }) }
     r.onsuccess = () => { resolve(r.result) }
   })
 
