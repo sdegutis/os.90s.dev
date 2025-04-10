09c87f031a73f909164505c60b35679c17a59139
sdegutis <sbdegutis@gmail.com>
2025-04-09 19:01:47 -0500

    Inline listdir.

--- site/sys/api/fs/sysfs.ts
+++ site/sys/api/fs/sysfs.ts
@@ -1,6 +1,6 @@
 import { files } from "./data.js"
 import { Drive } from "./drive.js"
-import { listdir } from "./util.js"
+
 
 export class SysDrive implements Drive {
 
@@ -29,4 +29,16 @@ export class SysDrive implements Drive {
     return false
   }
 
+}
+
+function listdir(full: string, entries: { path: string, content?: string }[]): string[] {
+  return entries
+    .map(file => file.content === undefined ? file.path + '/' : file.path)
+    .filter(path => path.startsWith(full))
+    .map(path => path.slice(full.length))
+    .filter(name => name)
+    .map(name => name.match(/^[^\/]+\/?/)![0])
+    .reduce((set, name) => set.add(name), new Set<string>())
+    .values()
+    .toArray()
 }
