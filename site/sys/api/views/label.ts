f7d8cc0038d8ae57b18ea37f5ab1bbaacb24f68b
sdegutis <sbdegutis@gmail.com>
2025-04-10 16:55:10 -0500

    Not needed.

--- site/sys/api/views/label.ts
+++ site/sys/api/views/label.ts
@@ -15,8 +15,8 @@ export class Label extends View {
   override init(): void {
     super.init()
 
-    this.$text.watch(() => { this.adjust(); this.needsRedraw() })
-    this.$font.watch(() => { this.adjust(); this.needsRedraw() })
+    this.$text.watch(() => this.adjust())
+    this.$font.watch(() => this.adjust())
     this.$color.watch(() => this.needsRedraw())
     this.adjust()
   }
