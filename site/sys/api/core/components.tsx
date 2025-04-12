aa60032a96ca011770170e2f15959820215abfa5
sdegutis <sbdegutis@gmail.com>
2025-04-12 16:46:04 -0500

    Hmm.

--- site/sys/api/core/components.tsx
+++ site/sys/api/core/components.tsx
@@ -5,6 +5,7 @@ import { PanelView } from "../components/panel.js"
 import { TextFieldComp } from "../components/textfield.js"
 
 export const components: Record<string, Component<any>> = Object.create(null)
+
 export type Component<T extends Record<string, any>> = (data: T) => JSX.Element
 
 components['button'] = ButtonComp
