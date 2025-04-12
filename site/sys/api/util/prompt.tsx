5259043356f18caac3149c7137c3db3fff7519da
sdegutis <sbdegutis@gmail.com>
2025-04-12 16:02:06 -0500

    Good cleanup?

--- site/sys/api/util/prompt.tsx
+++ site/sys/api/util/prompt.tsx
@@ -3,14 +3,13 @@ import { TextModel } from "../text/model.js"
 import { Border } from "../views/border.js"
 import { Center } from "../views/center.js"
 import { GroupX, GroupY } from "../views/group.js"
-import { Label } from "../views/label.js"
 
 export async function showPrompt(panel: Panel, text: string) {
   const result = Promise.withResolvers<string | null>()
 
   const model = new TextModel()
 
-  const prompt = <Label text={text} />
+  const prompt = <>{text}</>
   const buttons = <GroupX gap={2}>
     <button action={no} style='cancel' text={'cancel'} />
     <button action={ok} style='submit' text={'ok'} />
@@ -19,31 +18,32 @@ export async function showPrompt(panel: Panel, text: string) {
   const length = Math.max(buttons.size.w, prompt.size.w)
   const field = <textfield length={length} model={model} autofocus onEnter={ok} />
 
-  const dialog = <Border
-    canMouse
-    onKeyPress={key => {
-      if (key === 'enter') ok()
-      if (key === 'escape') no()
-      return true
-    }}
-    background={0x222222ff} padding={1} paddingColor={0x005599ff}>
-    <Border padding={3}>
-      <GroupY align={'m'} gap={4}>
-        <Border padding={2}>
-          {prompt}
-        </Border>
-        {field}
-        {buttons}
-      </GroupY>
-    </Border>
-  </Border>
-
   const host = <Center
     canMouse
     size={panel.root.$size}
     background={0x00000077}
     onMouseDown={no}
-  >{dialog}</Center>
+  >
+    <Border
+      canMouse
+      onKeyPress={key => {
+        if (key === 'enter') ok()
+        if (key === 'escape') no()
+        return true
+      }}
+      background={0x222222ff} padding={1} paddingColor={0x005599ff}
+    >
+      <Border padding={3}>
+        <GroupY align={'m'} gap={4}>
+          <Border padding={2}>
+            {prompt}
+          </Border>
+          {field}
+          {buttons}
+        </GroupY>
+      </Border>
+    </Border>
+  </Center>
 
   const close = () => panel.root.children = panel.root.children.filter(c => c !== host)
   function ok() { close(); result.resolve(model.getText()) }
