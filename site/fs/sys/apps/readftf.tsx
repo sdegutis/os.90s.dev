f86bcb201b2136bea7c795dd3956f1462e6ee75c
sdegutis <sbdegutis@gmail.com>
2025-04-12 10:10:27 -0500

    WIP readftf

--- site/fs/sys/apps/readftf.tsx
+++ site/fs/sys/apps/readftf.tsx
@@ -1,7 +1,7 @@
 import * as api from "/api.js"
 
 let content = ''
-const $filepath = api.$<string | undefined>(api.program.opts["file"])
+const $filepath = api.$<string>(api.program.opts["file"] ?? '')
 if ($filepath.val) content = await api.fs.getFile($filepath.val) ?? ''
 
 const model = new api.TextModel(content)
@@ -11,23 +11,49 @@ model.highlighter = hl
 // hl.log = true
 hl.highlight(model, 0)
 
+const pathModel = new api.TextModel($filepath.val)
+
+function changePath() {
+
+}
+
 const panel = await api.Panel.create({ name: "readftf" },
-  <api.FilePanelView filedata={() => model.getText()} filepath={$filepath} title='readftf' size={{ w: 100, h: 70 }}>
-    <api.Scroll
-      background={0xffffff11}
-      onMouseDown={function (b) { this.content.onMouseDown?.(b) }}
-      onMouseMove={function (p) { this.content.onMouseMove?.(p) }}
-      onMouseUp={function () { this.content.onMouseUp?.() }}
-    >
-      <api.Border padding={4}
-        onMouseDown={function (b) { this.firstChild!.onMouseDown?.(b) }}
-        onMouseMove={function (p) { this.firstChild!.onMouseMove?.(p) }}
-        onMouseUp={function () { this.firstChild!.onMouseUp?.() }}
+  <api.PanelView title='readftf' size={{ w: 100, h: 70 }}>
+    <api.PanedYA>
+
+      <api.PanedXA>
+        <api.GroupX>
+          <api.Button padding={2}><api.Label text='<' /></api.Button>
+          <api.Button padding={2}><api.Label text='>' /></api.Button>
+        </api.GroupX>
+        <api.GroupX>
+          <api.Button padding={2}><api.Label text='<' /></api.Button>
+          <api.Button padding={2}><api.Label text='>' /></api.Button>
+        </api.GroupX>
+        {/* <api.Margin padding={2}>
+          <api.Scroll>
+            <api.TextBox onEnter={changePath} model={pathModel} />
+          </api.Scroll>
+        </api.Margin> */}
+      </api.PanedXA>
+
+      <api.Scroll
+        background={0xffffff11}
+        onMouseDown={function (b) { this.content.onMouseDown?.(b) }}
+        onMouseMove={function (p) { this.content.onMouseMove?.(p) }}
+        onMouseUp={function () { this.content.onMouseUp?.() }}
       >
-        <api.TextBox editable={false} model={model} autofocus />
-      </api.Border>
-    </api.Scroll>
-  </api.FilePanelView>
+        <api.Border padding={4}
+          onMouseDown={function (b) { this.firstChild!.onMouseDown?.(b) }}
+          onMouseMove={function (p) { this.firstChild!.onMouseMove?.(p) }}
+          onMouseUp={function () { this.firstChild!.onMouseUp?.() }}
+        >
+          <api.TextBox editable={false} model={model} autofocus />
+        </api.Border>
+      </api.Scroll>
+
+    </api.PanedYA>
+  </api.PanelView>
 )
 
 panel.focusPanel()
