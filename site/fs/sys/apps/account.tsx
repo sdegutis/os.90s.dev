e9a08c4a4f76e51b967c9fc7f0d53f729077daae
sdegutis <sbdegutis@gmail.com>
2025-04-12 15:21:06 -0500

    Tidy?

--- site/fs/sys/apps/account.tsx
+++ site/fs/sys/apps/account.tsx
@@ -1,52 +1,43 @@
 import * as api from '/api.js'
 
+const panel = await api.sys.makePanel({ name: 'account' },
+  <api.PanelView title='account' size={{ w: 150, h: 120 }}>
+    <api.Margin children={api.$userState.adapt(state => {
+      switch (state.type) {
+        case 'guest': return [<SigninView />]
+        case 'registering': return [<RegisterView state={state} />]
+        case 'verifying': return [<VerifyView state={state} />]
+        case 'known': return [<WelcomeView state={state} />]
+      }
+    })} />
+  </api.PanelView>
+)
 
+panel.focusPanel()
 
-
-function SimpleForm(data: {
-  label: string,
-  buttons: Record<string, (value: string) => Promise<string | void>>,
-}) {
-  const $error = api.$('')
-
-  const model = new api.TextModel()
-
-  function wrapSubmit(onSubmit: (val: string) => Promise<string | void>) {
-    return async () => {
-      const error = await onSubmit(model.getText())
-      if (error) $error.val = error
-    }
+function WelcomeView({ state }: { state: api.UserStateKnown }) {
+  async function logout() {
+    const [err] = await api.POST('/user/logout', '')
+    if (err) { console.error(err); return }
+    api.updateAccountFromServer()
   }
 
-  const defaultButton = Object.values(data.buttons).at(-1)!
-
   return (
-    <api.Center>
-      <api.GroupY gap={4}>
+    <api.PanedYA>
 
-        <api.GroupX>
-          <api.Border padding={2}>
-            <api.Label color={0xffffff33} text={data.label} />
-          </api.Border>
-          <textfield model={model} onEnter={wrapSubmit(defaultButton)} autofocus />
-        </api.GroupX>
+      <api.GroupX background={0x00000033}>
+        <api.Button onClick={logout} padding={2}>
+          <api.Label text='logout' />
+        </api.Button>
+      </api.GroupX>
 
-        <api.GroupX
-          gap={2}
-          children={Object.entries(data.buttons).map(([label, onSubmit]) => {
-            return <api.Button padding={2} onClick={wrapSubmit(onSubmit)} background={0xffffff11}>
-              <api.Label text={label} />
-            </api.Button>
-          })}
-        />
+      <api.Center>
+        <>Welcome, {state.username}!</>
+      </api.Center>
 
-        <api.Label text={$error} color={0x99000099} />
-
-      </api.GroupY>
-    </api.Center>
+    </api.PanedYA>
   )
 }
-
 
 function SigninView() {
   return <SimpleForm
@@ -98,43 +89,45 @@ function VerifyView({ state }: { state: api.UserStateVerifying }) {
   />
 }
 
-function WelcomeView({ state }: { state: api.UserStateKnown }) {
+function SimpleForm(data: {
+  label: string,
+  buttons: Record<string, (value: string) => Promise<string | void>>,
+}) {
+  const $error = api.$('')
+  const model = new api.TextModel()
 
-  async function logout() {
-    const [err] = await api.POST('/user/logout', '')
-    if (err) { console.error(err); return }
-    api.updateAccountFromServer()
+  function wrapSubmit(onSubmit: (val: string) => Promise<string | void>) {
+    return async () => {
+      const error = await onSubmit(model.getText())
+      if (error) $error.val = error
+    }
   }
 
+  const lastOnSubmit = Object.values(data.buttons).at(-1)!
+
   return (
-    <api.PanedYA>
+    <api.Center>
+      <api.GroupY gap={4}>
 
-      <api.GroupX background={0x00000033}>
-        <api.Button onClick={logout} padding={2}>
-          <api.Label text='logout' />
-        </api.Button>
-      </api.GroupX>
+        <api.GroupX>
+          <api.Border padding={2}>
+            <api.Label color={0xffffff33} text={data.label} />
+          </api.Border>
+          <textfield model={model} onEnter={wrapSubmit(lastOnSubmit)} autofocus />
+        </api.GroupX>
 
-      <api.Center>
-        <>Welcome, {state.username}!</>
-      </api.Center>
+        <api.GroupX
+          gap={2}
+          children={Object.entries(data.buttons).map(([label, onSubmit]) => {
+            return <api.Button padding={2} onClick={wrapSubmit(onSubmit)} background={0xffffff11}>
+              <api.Label text={label} />
+            </api.Button>
+          })}
+        />
 
-    </api.PanedYA>
+        <api.Label text={$error} color={0x99000099} />
+
+      </api.GroupY>
+    </api.Center>
   )
-
 }
-
-const panel = await api.sys.makePanel({ name: 'account' },
-  <api.PanelView title='account' size={{ w: 150, h: 120 }}>
-    <api.Margin children={api.$userState.adapt(state => {
-      switch (state.type) {
-        case 'guest': return [<SigninView />]
-        case 'registering': return [<RegisterView state={state} />]
-        case 'verifying': return [<VerifyView state={state} />]
-        case 'known': return [<WelcomeView state={state} />]
-      }
-    })} />
-  </api.PanelView>
-)
-
-panel.focusPanel()
