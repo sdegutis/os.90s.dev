1881023a153f04f09dcda46cb65915dba3ffafee
sdegutis <sbdegutis@gmail.com>
2025-04-14 12:46:36 -0500

    Grid?

--- site/fs/sys/docs/readme.page.tsx
+++ site/fs/sys/docs/readme.page.tsx
@@ -1,13 +1,14 @@
 import { Browser, DocsPage } from "./common.js"
-import { fsPathOf, GroupY } from "/api.js"
+import { fsPathOf, Grid, Label, View } from "/api.js"
 
 export default (browser: Browser) => {
   return <DocsPage browser={browser} current={fsPathOf(import.meta.url)}>
-    <GroupY>
+    <Grid xgap={2} ygap={4}>
+      {...[<Label text={'hi'} />, <Label text={'ho'} />] as View[]}
       <>hello world</>
       <button action={() => {
         browser.load(import.meta.resolve('./readme2.page.js'))
       }}>load other page</button>
-    </GroupY>
+    </Grid>
   </DocsPage>
 }
