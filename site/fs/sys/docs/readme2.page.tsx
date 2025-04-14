a948c33661343a3e3d5616f2be6d9c3959d4291f
sdegutis <sbdegutis@gmail.com>
2025-04-14 15:06:51 -0500

    Fix.

--- site/fs/sys/docs/readme2.page.tsx
+++ site/fs/sys/docs/readme2.page.tsx
@@ -1,11 +1,11 @@
 import { Browser } from "../apps/browser.app.js"
 import { DocsPage } from "./common.js"
-import { fsPathOf, GroupY } from "/api.js"
+import { fsPathOf, GroupY, Label } from "/api.js"
 
 export default (browser: Browser) => {
   return <DocsPage browser={browser} current={fsPathOf(import.meta.url)}>
     <GroupY>
-      <>hello world 2</>
+      <Label text='hello world!' />
       <button action={() => {
         browser.load(import.meta.resolve('./readme.page.js'))
       }}>go back page</button>
