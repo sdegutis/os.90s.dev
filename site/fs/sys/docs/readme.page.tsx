8be3d7ccd16585808a900e8c5d6f1bc36fa9f2a1
sdegutis <sbdegutis@gmail.com>
2025-04-14 13:15:38 -0500

    Not sure...

--- site/fs/sys/docs/readme.page.tsx
+++ site/fs/sys/docs/readme.page.tsx
@@ -1,15 +1,22 @@
 import { Browser, DocsPage } from "./common.js"
-import { fsPathOf, Grid, Label } from "/api.js"
+import { fsPathOf, Label } from "/api.js"
 
 export default (browser: Browser) => {
   return <DocsPage browser={browser} current={fsPathOf(import.meta.url)}>
-    <Grid xgap={2} ygap={4} flow>
+    <>
+      hello
+      {'\n'}
+      how are you?
+
+
       <Label text={'hi'} />
       <Label text={'ho'} />
       <>hello world</>
+
+      this is cool
       <button action={() => {
         browser.load(import.meta.resolve('./readme2.page.js'))
       }}>load other page</button>
-    </Grid>
+    </>
   </DocsPage>
 }
--- site/sys/api/composites/implicit.tsx
+++ site/sys/api/composites/implicit.tsx
@@ -1,8 +1,26 @@
+import { Ref } from "../core/ref.js"
+import { Grid } from "../views/grid.js"
 import { Label } from "../views/label.js"
+import { View } from "../views/view.js"
 
-export function ImplicitComp(data: { children: string | string[] }) {
-  const str = typeof data.children === 'string'
-    ? data.children
-    : data.children.join('')
-  return <Label text={str} />
+export function ImplicitComp(data: { children: any }) {
+  let children = data.children
+
+  if (!(children instanceof Ref)) {
+    if (!(children instanceof Array)) children = [children]
+    const normalized: View[] = []
+
+    for (const child of children) {
+      if (typeof child === 'string') {
+        normalized.push(<Label text={child} />)
+      }
+      else {
+        normalized.push(child)
+      }
+    }
+
+    children = normalized
+  }
+
+  return <Grid flow xgap={3} ygap={1} children={children} />
 }
