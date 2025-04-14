4fad0e44c5a0fb5cfb453ef36de7bfddb4c949aa
sdegutis <sbdegutis@gmail.com>
2025-04-14 12:45:55 -0500

    Scroll sidebar.

--- site/fs/sys/docs/common.tsx
+++ site/fs/sys/docs/common.tsx
@@ -1,4 +1,4 @@
-import { Button, GroupY, Label, PanedXA, View } from "/api.js"
+import { Button, GroupY, Label, Scroll, SplitXA, View } from "/api.js"
 
 export interface Browser {
   load(str: string): void
@@ -10,20 +10,22 @@ const pages = {
 }
 
 function Sidebar(data: { current: string, browser: Browser }) {
-  return <GroupY align={'+'} background={0x00000033}>
-    {Object.entries(pages).map(([title, path]) => {
-      return <Button padding={2} onClick={() => data.browser.load(path)}>
-        <Label text={title} color={data.current === path
-          ? 0x0099ffff
-          : 0xccccccff} />
-      </Button>
-    })}
-  </GroupY>
+  return <Scroll background={0x00000033} showh={false}>
+    <GroupY align={'+'}>
+      {Object.entries(pages).map(([title, path]) => {
+        return <Button padding={2} onClick={() => data.browser.load(path)}>
+          <Label text={title} color={data.current === path
+            ? 0x0099ffff
+            : 0xccccccff} />
+        </Button>
+      })}
+    </GroupY>
+  </Scroll>
 }
 
 export function DocsPage(data: { current: string, browser: Browser, children: View }) {
-  return <PanedXA>
+  return <SplitXA pos={30}>
     <Sidebar current={data.current} browser={data.browser} />
     {data.children}
-  </PanedXA>
+  </SplitXA>
 }
