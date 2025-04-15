e1f1b7d8dbd2f3091b01cee615cbd2b8f07338af
sdegutis <sbdegutis@gmail.com>
2025-04-15 07:37:57 -0500

    Idea...

--- site/fs/sys/docs/readme.page.tsx
+++ site/fs/sys/docs/readme.page.tsx
@@ -1,23 +1,58 @@
 import { Browser } from "../apps/browser.app.js"
 import { DocsPage } from "./common.js"
-import { fsPathOf, Label } from "/api.js"
+import { fsPathOf, View } from "/api.js"
 
 export default (browser: Browser) => {
   return <DocsPage browser={browser} current={fsPathOf(import.meta.url)}>
-    <>
-      hello
-      {'\n'}
-      how are you?
+    {lang`
+      # this is a header
 
-
-      <Label text={'hi'} />
-      <Label text={'ho'} />
-      <>hello world</>
-
-      this is cool
-      <button action={() => {
-        browser.load(import.meta.resolve('./readme2.page.js'))
-      }}>load other page</button>
-    </>
+      hello world
+      *very* cool
+      
+      yes it /is!/
+      
+      > some code
+      > example
+      
+      and also
+      
+      "" some block quotes
+      "" from some people
+      
+      ### another section${<button action={() => { }}>hi</button>} ###
+      
+      - very
+      - good
+      
+      ${<button action={() => { browser.load(import.meta.resolve('./readme2.page.js')) }}>
+        load other page
+      </button>}
+      
+      * lists?
+      * not sure
+    `}
   </DocsPage>
+}
+
+class Flow extends View {
+
+  override adjust(): void {
+    const w = this.parent?.size.w
+    console.log('in here', w)
+  }
+
+}
+
+function lang(array: TemplateStringsArray, ...args: any[]) {
+
+  for (let i = 0; i < array.length; i++) {
+    const chunk = array[i]
+    const arg = args[i]
+
+    console.log([chunk])
+    console.log([arg])
+  }
+
+  return <View />
 }
--- site/sys/api/composites/implicit.tsx
+++ site/sys/api/composites/implicit.tsx
@@ -1,38 +1,8 @@
-import { Ref } from "../core/ref.js"
 import { Label } from "../views/label.js"
-import { View } from "../views/view.js"
 
-class Flow extends View {
-
-  override adjust(): void {
-    const w = this.parent?.size.w
-    console.log('in here', w)
-  }
-
-}
-
-export function ImplicitComp(data: { children: any }) {
-  let children = data.children
-
-  if (!(children instanceof Ref)) {
-    if (!(children instanceof Array)) children = [children]
-    const normalized: View[] = []
-
-    for (const child of children) {
-      if (typeof child === 'string') {
-        for (const line of child.split('\n')) {
-          for (const word of line.split(/[\t \r]/)) {
-            normalized.push(<Label text={word} />)
-          }
-        }
-      }
-      else {
-        normalized.push(child)
-      }
-    }
-
-    children = normalized
-  }
-
-  return <Flow children={children} />
+export function ImplicitComp(data: { children: string | string[] }) {
+  const str = typeof data.children === 'string'
+    ? data.children
+    : data.children.join('')
+  return <Label text={str} />
 }
