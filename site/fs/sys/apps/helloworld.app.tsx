2a8d774e4cde473f064c196a6d9f21c6ecc3b365
sdegutis <sbdegutis@gmail.com>
2025-04-18 09:37:08 -0500

    hello world

---
+++ site/fs/sys/apps/helloworld.app.tsx
@@ -0,0 +1,21 @@
+import * as api from '/api.js'
+await api.appReady
+
+const $count = api.$(0)
+const inc = () => $count.val++
+
+const panel = await api.sys.makePanel({ name: "hello world" },
+  <panel size={{ w: 70, h: 50 }}>
+    <api.Center>
+      <api.GroupY gap={4}>
+        <api.Label text='hello world!' />
+        <api.GroupX gap={2}>
+          <button style='submit' action={inc}>click me</button>
+          <api.Label text={$count.adapt(n => `clicked ${n} times`)} />
+        </api.GroupX>
+      </api.GroupY>
+    </api.Center>
+  </panel>
+)
+
+panel.focusPanel()
