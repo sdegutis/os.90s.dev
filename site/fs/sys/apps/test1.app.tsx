b97ab335870ff25645951dd542fa64e28b0d87cb
sdegutis <sbdegutis@gmail.com>
2025-04-18 09:58:33 -0500

    Better test.

--- site/fs/sys/apps/test1.app.tsx
+++ site/fs/sys/apps/test1.app.tsx
@@ -1,12 +1,30 @@
 import * as api from '/api.js'
 await api.appReady
 
+
 const panel = await api.sys.makePanel({ name: "test 1" },
   <panel size={{ w: 70, h: 50 }}>
-    <api.Border padding={1} paddingColor={0xffffff11} background={0x99000099}>
-      <api.Margin padding={1} paddingColor={0xffffff11} point={{ x: 10, y: 10 }} size={{ w: 50, h: 50 }} background={0x00990099}>
-
-      </api.Margin>
+    <api.Border
+      padding={1}
+      paddingColor={0xffffff11}
+      background={0x99000099}
+      layout={() => { }}
+    >
+      <api.Margin
+        canMouse
+        onMouseDown={function (b) {
+          const m = this.panel!.$mouse
+          const done = b
+            ? api.debounce(api.dragResize(m, this.$size))
+            : api.debounce(api.dragMove(m, this.$point))
+          this.onMouseUp = done
+        }}
+        padding={1}
+        paddingColor={0xffffff11}
+        point={{ x: 10, y: 10 }}
+        size={{ w: 50, h: 50 }}
+        background={0x00990099}
+      />
     </api.Border>
   </panel>
 )
