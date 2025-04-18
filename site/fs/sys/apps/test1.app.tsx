c008b73356971d113baa973c805a40e42ffd4e99
sdegutis <sbdegutis@gmail.com>
2025-04-18 13:05:38 -0500

    Better test.

--- site/fs/sys/apps/test1.app.tsx
+++ site/fs/sys/apps/test1.app.tsx
@@ -5,7 +5,7 @@ await api.appReady
 const panel = await api.sys.makePanel({ name: "test 1" },
   <panel size={{ w: 70, h: 50 }}>
     <api.Margin name='a' padding={3}>
-      <api.Border name='b'
+      <api.Margin name='b'
         padding={1}
         paddingColor={0xffffff11}
         background={0x99000099}
@@ -24,7 +24,7 @@ const panel = await api.sys.makePanel({ name: "test 1" },
           size={{ w: 50, h: 50 }}
           background={0x00990099}
         />
-      </api.Border>
+      </api.Margin>
     </api.Margin>
   </panel>
 )
