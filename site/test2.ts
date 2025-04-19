e87cab514d394e37994f5fddfd0a30d2d5f78c7a
sdegutis <sbdegutis@gmail.com>
2025-04-19 16:23:23 -0500

    Points kinda work.

--- site/test2.ts
+++ site/test2.ts
@@ -64,46 +64,45 @@ function createProg(vert: string, frag: string) {
   return prog
 }
 
+
+
+new EventSource('/_reload').onmessage = () => location.reload()
+
+
+
+
+
 const vert = `#version 300 es
 in vec2 a_position;
-uniform vec2 u_resolution;
+uniform vec2 res;
 
 void main() {
-  vec2 zeroToOne = a_position / u_resolution;
-  vec2 zeroToTwo = zeroToOne * 2.0;
-  vec2 clipSpace = zeroToTwo - 1.0;
-  gl_Position = vec4(clipSpace * vec2(1,-1), 0, 1);
-  //gl_Position = a_position;
+  gl_Position = vec4((((a_position+0.5)/res*2.0)-1.0)*vec2(1,-1), 0, 1);
+  gl_PointSize = 1.0;
 }
 `
 
 const frag = `#version 300 es
 precision highp float;
-uniform vec4 u_color;
-out vec4 outColor;
+
+out vec4 color;
 
 void main() {
-  outColor = u_color;
+  color = vec4(0,.3,0,1);
 }
 `
 
 const prog = createProg(vert, frag)
 
-
-const resUniLoc = gl.getUniformLocation(prog, 'u_resolution')
-const colLoc = gl.getUniformLocation(prog, 'u_color')
+const resUniLoc = gl.getUniformLocation(prog, 'res')
 const posAttrLoc = gl.getAttribLocation(prog, 'a_position')
 
 const posBuf = gl.createBuffer()
 gl.bindBuffer(gl.ARRAY_BUFFER, posBuf)
 
 gl.bufferData(gl.ARRAY_BUFFER, new Float32Array([
-  10, 20,
-  80, 20,
-  10, 30,
-  10, 30,
-  80, 20,
-  80, 30,
+  319, 179,
+  0, 3,
 ]), gl.STATIC_DRAW)
 
 const vao = gl.createVertexArray()
@@ -120,8 +119,72 @@ gl.clear(gl.COLOR_BUFFER_BIT)
 gl.useProgram(prog)
 
 gl.uniform2f(resUniLoc, 320, 180)
-gl.uniform4f(colLoc, 0, 0.7, 0, 1)
 
 gl.bindVertexArray(vao)
 
-gl.drawArrays(gl.TRIANGLES, 0, 6)
+gl.drawArrays(gl.POINTS, 0, 2)
+
+
+
+
+// const vert = `#version 300 es
+// in vec2 a_position;
+// uniform vec2 u_resolution;
+
+// void main() {
+//   vec2 zeroToOne = a_position / u_resolution;
+//   vec2 zeroToTwo = zeroToOne * 2.0;
+//   vec2 clipSpace = zeroToTwo - 1.0;
+//   gl_Position = vec4(clipSpace * vec2(1,-1), 0, 1);
+//   //gl_Position = a_position;
+// }
+// `
+
+// const frag = `#version 300 es
+// precision highp float;
+// uniform vec4 u_color;
+// out vec4 outColor;
+
+// void main() {
+//   outColor = u_color;
+// }
+// `
+
+// const prog = createProg(vert, frag)
+
+
+// const resUniLoc = gl.getUniformLocation(prog, 'u_resolution')
+// const colLoc = gl.getUniformLocation(prog, 'u_color')
+// const posAttrLoc = gl.getAttribLocation(prog, 'a_position')
+
+// const posBuf = gl.createBuffer()
+// gl.bindBuffer(gl.ARRAY_BUFFER, posBuf)
+
+// gl.bufferData(gl.ARRAY_BUFFER, new Float32Array([
+//   10, 20,
+//   80, 20,
+//   10, 30,
+//   10, 30,
+//   80, 20,
+//   80, 30,
+// ]), gl.STATIC_DRAW)
+
+// const vao = gl.createVertexArray()
+// gl.bindVertexArray(vao)
+
+// gl.enableVertexAttribArray(posAttrLoc)
+// gl.vertexAttribPointer(posAttrLoc, 2, gl.FLOAT, false, 0, 0)
+
+// gl.viewport(0, 0, 320, 180)
+
+// gl.clearColor(0, 0, 0, 0)
+// gl.clear(gl.COLOR_BUFFER_BIT)
+
+// gl.useProgram(prog)
+
+// gl.uniform2f(resUniLoc, 320, 180)
+// gl.uniform4f(colLoc, 0, 0.7, 0, 1)
+
+// gl.bindVertexArray(vao)
+
+// gl.drawArrays(gl.TRIANGLES, 0, 6)
