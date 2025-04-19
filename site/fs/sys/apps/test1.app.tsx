8ccfcb32f8abde25ca22679e142571f0213cd4c2
sdegutis <sbdegutis@gmail.com>
2025-04-18 19:28:08 -0500

    Preparing to fix scroll.

--- site/fs/sys/apps/test1.app.tsx
+++ site/fs/sys/apps/test1.app.tsx
@@ -4,26 +4,27 @@ await api.appReady
 
 const panel = await api.sys.makePanel({ name: "test 1" },
   <panel size={{ w: 70, h: 50 }}>
-    <api.Margin name='a' padding={3}>
-      <api.Margin name='b'
+    <api.Margin padding={3}>
+      <api.Margin
         padding={1}
         paddingColor={0xffffff11}
         background={0x99000099}
-        layout={() => { }}
       >
-        <api.Margin name='c'
-          canMouse
-          onMouseDown={function (b) {
-            this.onMouseUp = b
-              ? api.debounce(api.dragResize(api.sys.$mouse, this.$size))
-              : api.debounce(api.dragMove(api.sys.$mouse, this.$point))
-          }}
-          padding={1}
-          paddingColor={0xffffff11}
-          point={{ x: 10, y: 10 }}
-          size={{ w: 50, h: 50 }}
-          background={0x00990099}
-        />
+        <api.Scroll background={0x00009999}>
+          <api.Margin
+            canMouse
+            onMouseDown={function (b) {
+              this.onMouseUp = b
+                ? api.debounce(api.dragResize(api.sys.$mouse, this.$size))
+                : api.debounce(api.dragMove(api.sys.$mouse, this.$point))
+            }}
+            padding={1}
+            paddingColor={0xffffff11}
+            point={{ x: 10, y: 10 }}
+            size={{ w: 50, h: 50 }}
+            background={0x00990099}
+          />
+        </api.Scroll>
       </api.Margin>
     </api.Margin>
   </panel>
