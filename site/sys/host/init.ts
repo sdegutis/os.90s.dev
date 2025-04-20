bf06a493543a377022611147c4131c6e99716f1a
sdegutis <sbdegutis@gmail.com>
2025-04-20 08:50:53 -0500

    Cool.

--- site/sys/host/init.ts
+++ site/sys/host/init.ts
@@ -4,3 +4,37 @@ await navigator.serviceWorker.register('/sw.js', { type: 'classic', updateViaCac
 await navigator.serviceWorker.ready
 
 await Sys.init()
+
+// new EventSource('/_reload').onmessage = () => location.reload()
+
+// const code = `
+
+// const stream1 = new Blob([code]).stream().pipeThrough(new CompressionStream('gzip'))
+// const blob1 = await new Response(stream1).blob()
+// const bytes1 = new Uint8Array(await blob1.arrayBuffer())
+// const compressed = btoa(String.fromCharCode(...bytes1))
+// console.log('hey', compressed)
+
+// const out1 = atob(compressed)
+// const out2 = new Uint8Array(Array(out1.length).keys().map(i => out1.charCodeAt(i)))
+// const out3 = new Blob([out2]).stream().pipeThrough(new DecompressionStream('gzip'))
+// const out4 = await new Response(out3).text()
+
+// console.log(out4 === code)
+
+// `
+
+// const stream1 = new Blob([code]).stream().pipeThrough(new CompressionStream('gzip'))
+// const blob1 = await new Response(stream1).blob()
+// const bytes1 = new Uint8Array(await blob1.arrayBuffer())
+// const compressed = btoa(String.fromCharCode(...bytes1))
+// console.log('hey', compressed)
+
+// const out1 = atob(compressed)
+// const out2 = new Uint8Array(Array(out1.length).keys().map(i => out1.charCodeAt(i)))
+// const out3 = new Blob([out2]).stream().pipeThrough(new DecompressionStream('gzip'))
+// const out4 = await new Response(out3).text()
+
+// console.log(out4 === code)
+
+// console.log(code.length, compressed.length)
