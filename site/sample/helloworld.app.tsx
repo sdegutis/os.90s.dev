ea6e1cc7fd62e4dbbdf660499764bb2cf7dad71c
sdegutis <sbdegutis@gmail.com>
2025-04-22 10:28:59 -0500

    Better sample.

--- site/sample/helloworld.app.tsx
+++ site/sample/helloworld.app.tsx
@@ -5,15 +5,17 @@ const $count = api.$(0)
 const inc = () => $count.val++
 
 const panel = await api.sys.makePanel({ name: "hello world" },
-  <api.Center size={api.sys.size} background={0x444444ff}>
-    <api.GroupY gap={4}>
-      <api.Label text='hello world!' />
-      <api.GroupX gap={2}>
-        <button style='submit' action={inc}>click me</button>
-        <api.Label text={$count.adapt(n => `clicked ${n} times`)} />
-      </api.GroupX>
-    </api.GroupY>
-  </api.Center>
+  <panel size={{ w: 120, h: 120 }}>
+    <api.Center>
+      <api.GroupY gap={4}>
+        <api.Label text='hello world!' />
+        <api.GroupX gap={2}>
+          <button style='submit' action={inc}>click me</button>
+          <api.Label text={$count.adapt(n => `clicked ${n} times`)} />
+        </api.GroupX>
+      </api.GroupY>
+    </api.Center>
+  </panel>
 )
 
 panel.focusPanel()
