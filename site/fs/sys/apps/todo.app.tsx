c636996efe7605a43b3eba61dad15cc30f5de030
sdegutis <sbdegutis@gmail.com>
2025-04-22 06:26:39 -0500

    Fixes.

--- site/fs/sys/apps/todo.app.tsx
+++ site/fs/sys/apps/todo.app.tsx
@@ -6,12 +6,12 @@ interface Item {
   done: api.Ref<boolean>
 }
 
-const model = new api.TextModel()
+const textModel = new api.TextModel()
 
-const $items = api.$<Item[]>([])
+const $items = api.$<Item[]>([], api.arrayEquals)
 const $itemViews = $items.adapt<api.View[]>(items => {
   return items.map(item => <ItemView item={item} />)
-})
+}, api.arrayEquals)
 
 const panel = await api.sys.makePanel({ name: "todo" },
   <panel size={{ w: 70, h: 50 }}>
@@ -25,7 +25,7 @@ const panel = await api.sys.makePanel({ name: "todo" },
           <api.GroupY align={'a'} children={$itemViews} />
         </api.Scroll>
         <api.Border padding={2} background={0x00000033}>
-          <api.TextBox model={model} autofocus onEnter={add} />
+          <api.TextBox model={textModel} autofocus onEnter={add} />
         </api.Border>
       </api.PanedYB>
     </api.PanedYA>
@@ -35,15 +35,15 @@ const panel = await api.sys.makePanel({ name: "todo" },
 panel.focusPanel()
 
 function add() {
-  const text = model.getText()
+  const text = textModel.getText()
   if (!text) return
 
   $items.val = [...$items.val, { done: api.$(false), text }]
-  model.setText('')
+  textModel.setText('')
 }
 
 function clear() {
-  $items.val = $items.val.filter(it => !it.done)
+  $items.val = $items.val.filter(it => !it.done.val)
 }
 
 function inverse() {
@@ -55,11 +55,14 @@ function ItemView({ item }: { item: Item }) {
     item.done.val = !item.done.val
   }
 
+  const $color = item.done.adapt<number>(done => done ? 0xffffff33 : 0xffffff77)
+  const $marker = item.done.adapt<string>(done => done ? 'x' : 'o')
+
   return (
     <api.Button onClick={toggleDone}>
       <api.GroupX gap={2}>
-        <api.Label color={0xffffff77} text={item.done.adapt<string>(done => done ? 'x' : 'o')} />
-        <api.Label text={item.text} />
+        <api.Label color={$color} text={$marker} />
+        <api.Label color={$color} text={item.text} />
       </api.GroupX>
     </api.Button>
   )
