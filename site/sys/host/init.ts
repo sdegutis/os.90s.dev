9626f774807e00674e7e94acb91d87f10190c166
sdegutis <sbdegutis@gmail.com>
2025-04-23 06:52:30 -0500

    Bounce.

--- site/sys/host/init.ts
+++ site/sys/host/init.ts
@@ -1,6 +1,7 @@
 import { sysConfig } from "../api/core/config.js"
 import { $ } from "../api/core/ref.js"
 import { Size } from "../api/core/types.js"
+import { debounce } from "../api/util/throttle.js"
 import { Sys } from "./sys.js"
 
 await Sys.installServiceWorker()
@@ -10,7 +11,7 @@ const params = new URLSearchParams(location.search)
 if (params.has('embed')) {
   const currentSize = (): Size => ({ w: window.innerWidth / 2, h: window.innerHeight / 2 })
   const $size = $(currentSize())
-  new ResizeObserver(() => { $size.$ = currentSize() }).observe(document.body)
+  new ResizeObserver(debounce(() => { $size.$ = currentSize() })).observe(document.body)
 
   const sys = new Sys($size)
   sys.launch('run' + location.search, {}, [])
