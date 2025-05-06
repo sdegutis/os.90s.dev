8733e3b973c6bd5463f1df9d4d61882e188a2856
sdegutis <sbdegutis@gmail.com>
2025-05-06 13:34:24 -0500

    Upgrade.

--- main.ts
+++ main.ts
@@ -13,7 +13,12 @@ swc1 = swc1.replace(/\nexport function/g, 'function')
 swc1 = swc1.replace(/\nexport {[\s\S]+/, '')
 swc1 = swc1.replace('import.meta.url', `location.origin + '/sys/sw/'`)
 
-const tree = new immaculata.LiveTree('site', import.meta.url)
+const tree = new immaculata.FileTree('site', import.meta.url, {
+  exclude: (path, stat) =>
+    path === '/fs/api.ts' ||
+    path.includes('/out/') ||
+    path.endsWith('.tsbuildinfo')
+})
 
 const copyright = `Copyright ©️ ${new Date().getFullYear()}. You're welcome and encouraged to use and learn from all the code here.`
 
@@ -23,84 +28,84 @@ const iconlink = `<link rel="shortcut icon" href="${`data:image/svg+xml,${encode
 const NETHOST = isDev ? 'http://localhost:8088' : 'https://net.90s.dev'
 
 function processSite() {
-  return tree.processFiles(files => {
+  const files = immaculata.Pipeline.from(tree.files)
 
-    files.with('^/sample/').remove()
+  files.with('^/sample/').remove()
 
-    const apidts = files.with('^/sys/out/api/.+\.d\.ts$').copy()
+  const apidts = files.with('^/sys/out/api/.+\.d\.ts$').copy()
 
-    files.with('^/fs/api\.ts$').remove()
+  files.with('^/fs/api\.ts$').remove()
 
-    files.with(/\.tsx?$/).do(f => f.text = f.text.replace(/\r/g, ''))
+  files.with(/\.tsx?$/).do(f => f.text = f.text.replace(/\r/g, ''))
 
-    files.add('/sys/sw/wasm.js', swc1)
-    files.add('/sys/sw/wasm_bg.wasm', swc2)
+  files.add('/sys/sw/wasm.js', swc1)
+  files.add('/sys/sw/wasm_bg.wasm', swc2)
 
-    const dbfile = files.with('^/sys/api/util/db.ts$').all()[0]
-    files.add('/sys/sw/db.ts', dbfile.text.replace('export ', ''))
+  const dbfile = files.with('^/sys/api/util/db.ts$').all()[0]
+  files.add('/sys/sw/db.ts', dbfile.text.replace('export ', ''))
 
-    files.with('/out/').remove()
-    files.with('tsconfig\.json').remove()
-    files.with(/\.d\.ts$/).remove()
+  files.with('/out/').remove()
+  files.with('tsconfig\.json').remove()
+  files.with(/\.d\.ts$/).remove()
 
-    files.add('/sys/api/core/nethost.ts', `export const NETHOST = ${JSON.stringify(NETHOST)}`)
-    files.add('/sys/sw/nethost.ts', `const NETHOST = ${JSON.stringify(NETHOST)}`)
+  files.add('/sys/api/core/nethost.ts', `export const NETHOST = ${JSON.stringify(NETHOST)}`)
+  files.add('/sys/sw/nethost.ts', `const NETHOST = ${JSON.stringify(NETHOST)}`)
 
-    files.with(/\.js$/).do(file => { file.text = `// ${copyright}\n\n` + file.text })
-    files.with(/\.tsx?$/).do(file => { file.text = `// ${copyright}\n\n` + file.text })
-    files.with(/\.html$/).do(file => { file.text = `<!-- ${copyright} -->\n\n` + file.text })
+  files.with(/\.js$/).do(file => { file.text = `// ${copyright}\n\n` + file.text })
+  files.with(/\.tsx?$/).do(file => { file.text = `// ${copyright}\n\n` + file.text })
+  files.with(/\.html$/).do(file => { file.text = `<!-- ${copyright} -->\n\n` + file.text })
 
-    const src = files.with('^/sys/api/').copy()
+  const src = files.with('^/sys/api/').copy()
 
-    files.with(/\.tsx?$/).without('^/fs/sys/').do(compileTsx)
+  files.with(/\.tsx?$/).without('^/fs/sys/').do(compileTsx)
 
-    files.with(/\.tsx?$/).do(file => { file.path = file.path.replace(/\.tsx?$/, '.js') })
+  files.with(/\.tsx?$/).do(file => { file.path = file.path.replace(/\.tsx?$/, '.js') })
 
-    const defaultExport = "\nexport * as default from './api.js'"
+  const defaultExport = "\nexport * as default from './api.js'"
 
-    const apis = files.with('^/sys/api/').paths()
-    fs.writeFileSync('./site/fs/api.ts', apis.map(p => `export * from "..${p}"`).join('\n') + defaultExport)
-    files.add('/api.js', apis.map(p => `export * from "${p}"`).join('\n') + defaultExport)
+  const apis = files.with('^/sys/api/').paths()
+  fs.writeFileSync('./site/fs/api.ts', apis.map(p => `export * from "..${p}"`).join('\n') + defaultExport)
+  files.add('/api.js', apis.map(p => `export * from "${p}"`).join('\n') + defaultExport)
 
-    src.do(f => { files.add('/fs/sys/api' + f.path.slice('/sys/api'.length).replace(/\.tsx?$/, '.js'), f.text) })
+  src.do(f => { files.add('/fs/sys/api' + f.path.slice('/sys/api'.length).replace(/\.tsx?$/, '.js'), f.text) })
 
-    const sysdata = JSON.stringify((files
-      .with('^/fs/sys')
-      .all().map(f => ({
-        path: f.path.slice('/fs/sys/'.length),
-        content: f.text,
-      }))
-    ), null, 2)
-    files.add('/sys/api/fs/data.js', `export const files = ${sysdata}`)
+  const sysdata = JSON.stringify((files
+    .with('^/fs/sys')
+    .all().map(f => ({
+      path: f.path.slice('/fs/sys/'.length),
+      content: f.text,
+    }))
+  ), null, 2)
+  files.add('/sys/api/fs/data.js', `export const files = ${sysdata}`)
 
-    const paths = files.without('^/fs/sys')
+  const paths = files.without('^/fs/sys')
 
-    const datas = paths.without('\.js$|\.html$|\.wasm$').paths()
-      .map(path => `<link rel="preload" as="fetch" href="${path}" crossorigin="anonymous" />`)
+  const datas = paths.without('\.js$|\.html$|\.wasm$').paths()
+    .map(path => `<link rel="preload" as="fetch" href="${path}" crossorigin="anonymous" />`)
 
-    const modules = paths.with('\.js$').without('^/sys/sw/').paths()
-      .map(path => `<link rel="modulepreload" href="${path}" />`)
+  const modules = paths.with('\.js$').without('^/sys/sw/').paths()
+    .map(path => `<link rel="modulepreload" href="${path}" />`)
 
-    const toinsert = [...datas, /* ...modules, */ iconlink].map(s => `  ${s}`).join('\n')
-    files.with(/\.html$/).do(file => file.text = file.text.replace('<head>', `<head>\n${toinsert}`))
+  const toinsert = [...datas, /* ...modules, */ iconlink].map(s => `  ${s}`).join('\n')
+  files.with(/\.html$/).do(file => file.text = file.text.replace('<head>', `<head>\n${toinsert}`))
 
-    const apidts2 = Object.fromEntries(apidts.all().map(f => [f.path.replace(/^\/sys\/out\//, '/sys/'), f.text]))
-    apidts2['/api.d.ts'] = apis.map(p => `export * from "${p}"`).join('\n') + defaultExport
-    const apiexports = JSON.stringify(apidts2)
+  const apidts2 = Object.fromEntries(apidts.all().map(f => [f.path.replace(/^\/sys\/out\//, '/sys/'), f.text]))
+  apidts2['/api.d.ts'] = apis.map(p => `export * from "${p}"`).join('\n') + defaultExport
+  const apiexports = JSON.stringify(apidts2)
 
-    files.add('/api.d.ts.json', apiexports)
+  files.add('/api.d.ts.json', apiexports)
 
-    const zip = new Zip()
-    zip.addLocalFolder('node_modules/@types/wicg-file-system-access', 'node_modules/@types/wicg-file-system-access')
-    zip.addLocalFolder('site/sys/api/', 'sys/api/')
-    zip.addLocalFile('site/sys/tsconfig.json', 'sys/')
-    zip.addLocalFolder('site/sample/', 'app/')
-    zip.addLocalFolder('./site/fs/', 'fs', path => !path.match(/fs(\\|\/)out(\\|\/)/))
-    zip.addFile('.vscode/settings.json', Buffer.from(`{ "typescript.preferences.importModuleSpecifierEnding": "js" }`))
-    zip.addFile('sys/api/core/nethost.ts', Buffer.from(`export const NETHOST = ${JSON.stringify(NETHOST)}`))
-    files.add('/helloworld.zip', zip.toBuffer())
+  const zip = new Zip()
+  zip.addLocalFolder('node_modules/@types/wicg-file-system-access', 'node_modules/@types/wicg-file-system-access')
+  zip.addLocalFolder('site/sys/api/', 'sys/api/')
+  zip.addLocalFile('site/sys/tsconfig.json', 'sys/')
+  zip.addLocalFolder('site/sample/', 'app/')
+  zip.addLocalFolder('./site/fs/', 'fs', path => !path.match(/fs(\\|\/)out(\\|\/)/))
+  zip.addFile('.vscode/settings.json', Buffer.from(`{ "typescript.preferences.importModuleSpecifierEnding": "js" }`))
+  zip.addFile('sys/api/core/nethost.ts', Buffer.from(`export const NETHOST = ${JSON.stringify(NETHOST)}`))
+  files.add('/helloworld.zip', zip.toBuffer())
 
-  })
+  return files.results()
 }
 
 if (isDev) {
@@ -115,10 +120,7 @@ if (isDev) {
   server.files = await processSite()
   server.notFound = () => '/404.html'
 
-  tree.watch({}, async (paths) => {
-    if (paths.has('/fs/api.ts') && paths.size === 1) return
-    if ([...paths].every(p => p.endsWith('.tsbuildinfo'))) return
-
+  tree.watch().on('filesUpdated', async () => {
     const start = Date.now()
     try { server.files = await processSite() }
     catch (e) { console.error(e) }
