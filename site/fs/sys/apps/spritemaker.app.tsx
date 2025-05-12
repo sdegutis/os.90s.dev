9eb90fc00d8e08c6a7c44ef685eaccd0224022c8
sdegutis <sbdegutis@gmail.com>
2025-05-11 22:54:19 -0500

    Size labels.

--- site/fs/sys/apps/spritemaker.app.tsx
+++ site/fs/sys/apps/spritemaker.app.tsx
@@ -1,4 +1,4 @@
-import api, { $, Border, Button, Grid, GroupY, Label, Ref, View } from '/os/api.js'
+import api, { $, Border, Button, Grid, GroupX, GroupY, Label, Ref, Size, View } from '/os/api.js'
 await api.preludesFinished
 
 const $color = $(0x00000000)
@@ -6,12 +6,30 @@ const $color = $(0x00000000)
 const panel = await api.sys.makePanel({ name: "sprite maker" },
   <panel size={{ w: 120, h: 70 }}>
     <api.Center>
-      <ColorView $color={$color} />
+      <Border background={0xffffff11} padding={2} up={3}>
+        <GroupY>
+          <ColorView $color={$color} />
+          <SizeLabels $size={$<Size>({ w: 8, h: 8 })} />
+        </GroupY>
+      </Border>
     </api.Center>
   </panel>
 )
 
 panel.focusPanel()
+
+function SizeLabels(data: { $size: Ref<Size> }) {
+  return <GroupX gap={2}>
+    <GroupX gap={-1}>
+      <Label text='w:' color={0xffffff33} />
+      <Label text={data.$size.adapt(s => s.w.toString())} />
+    </GroupX>
+    <GroupX gap={-1}>
+      <Label text='h:' color={0xffffff33} />
+      <Label text={data.$size.adapt(s => s.h.toString())} />
+    </GroupX>
+  </GroupX>
+}
 
 function ColorView(data: { $color: Ref<number> }) {
 
@@ -97,10 +115,8 @@ function ColorView(data: { $color: Ref<number> }) {
     </Grid>
   }
 
-  return <Border background={0xffffff11} up={1}>
-    <GroupY>
-      <PalettePicker />
-      <ColorPicker />
-    </GroupY>
-  </Border>
+  return <GroupY>
+    <PalettePicker />
+    <ColorPicker />
+  </GroupY>
 }
