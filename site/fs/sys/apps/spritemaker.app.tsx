81b0e26b6acd67d862ddd9d3d625eb972c071e0d
sdegutis <sbdegutis@gmail.com>
2025-05-13 13:41:06 -0500

    Again.

--- site/fs/sys/apps/spritemaker.app.tsx
+++ site/fs/sys/apps/spritemaker.app.tsx
@@ -1,4 +1,4 @@
-import api, { $ } from '/os/api.js'
+import api, { $, Border, GroupX, GroupY, Label, Ref, type Size } from '/os/api.js'
 import { ColorView } from '/os/fs/sys/libs/colorpicker.js'
 await api.preludesFinished
 
@@ -13,8 +13,28 @@ const file = {
 
 const panel = await api.sys.makePanel({ name: "sprite maker" },
   <panel size={{ w: 220, h: 150 }} file={file}>
-    <ColorView $color={$color} />
+    <api.Center>
+      <Border background={0xffffff11} padding={2} up={3}>
+        <GroupY>
+          <ColorView $color={$color} />
+          <SizeLabels $size={$<Size>({ w: 8, h: 8 })} />
+        </GroupY>
+      </Border>
+    </api.Center>
   </panel>
 )
 
 panel.focusPanel()
+
+function SizeLabels(data: { $size: Ref<Size> }) {
+  return <GroupX gap={2}>
+    <GroupX gap={-1}>
+      <Label text='w:' color={0xffffff33} />
+      <Label text={data.$size.adapt(s => s.w.toString())} />
+    </GroupX>
+    <GroupX gap={-1}>
+      <Label text='h:' color={0xffffff33} />
+      <Label text={data.$size.adapt(s => s.h.toString())} />
+    </GroupX>
+  </GroupX>
+}
