bcfdcff405359d30b1295f2fb391bedaef2f5385
sdegutis <sbdegutis@gmail.com>
2025-05-20 11:44:51 -0500

    Starting on canvas.

--- site/fs/sys/apps/paint.app.tsx
+++ site/fs/sys/apps/paint.app.tsx
@@ -1,9 +1,16 @@
-import api, { $, Border, Button, GroupX, GroupY, Label, PanedXB, Ref, Scroll, View, type Size } from '/os/api.js'
+import api, { $, Border, Button, GroupX, GroupY, Label, multiplex, PanedXB, Ref, Scroll, View, type Size } from '/os/api.js'
 import { drawPinStripes } from '/os/fs/sys/libs/draw.js'
 await api.preludesFinished
 
 const $color = $(0x00000000)
 const $grid = $(true)
+
+const $size = $<Size>({ w: 24, h: 24 })
+const $zoom = $(2)
+
+const $canvasSize = multiplex([$size, $zoom], (size, zoom) => {
+  return { w: size.w * zoom, h: size.h * zoom }
+})
 
 const file = {
   $path: $(api.program.opts["file"] ?? 'usr/untitled1.bmp.jsln'),
@@ -16,7 +23,7 @@ const panel = await api.sys.makePanel({ name: "paint" },
   <panel size={{ w: 220, h: 150 }} file={file}>
     <PanedXB>
       <Scroll draw={drawPinStripes()}>
-        <View />
+        <View size={$canvasSize} background={0x00000033} />
       </Scroll>
       <GroupY background={0x333333ff}>
         <Border padding={2}>
