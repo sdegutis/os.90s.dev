9c8074f9fa0636e1de7a0469df5fa5aef4656824
sdegutis <sbdegutis@gmail.com>
2025-05-20 12:09:37 -0500

    Resizable.

--- site/fs/sys/apps/paint.app.tsx
+++ site/fs/sys/apps/paint.app.tsx
@@ -1,4 +1,4 @@
-import api, { $, Border, Button, GroupX, GroupY, Label, multiplex, PanedXB, Ref, Scroll, View, type Size } from '/os/api.js'
+import api, { $, Border, Button, dragResize, GroupX, GroupY, Label, multiplex, PanedXB, Ref, sys, View, type Size } from '/os/api.js'
 import { drawPinStripes } from '/os/fs/sys/libs/draw.js'
 await api.preludesFinished
 
@@ -8,9 +8,10 @@ const $grid = $(true)
 const $size = $<Size>({ w: 24, h: 24 })
 const $zoom = $(2)
 
-const $canvasSize = multiplex([$size, $zoom], (size, zoom) => {
-  return { w: size.w * zoom, h: size.h * zoom }
-})
+const $canvasSize = multiplex([$size, $zoom], (size, zoom) => ({
+  w: size.w * zoom,
+  h: size.h * zoom,
+}))
 
 const file = {
   $path: $(api.program.opts["file"] ?? 'usr/untitled1.bmp.jsln'),
@@ -22,14 +23,15 @@ const file = {
 const panel = await api.sys.makePanel({ name: "paint" },
   <panel size={{ w: 220, h: 150 }} file={file}>
     <PanedXB>
-      <Scroll draw={drawPinStripes()}>
-        <View size={$canvasSize} background={0x00000033} />
-      </Scroll>
+      <View draw={drawPinStripes()}>
+        <CanvasView grid={$grid} canvasSize={$canvasSize} />
+        <Resizer canvasSize={$canvasSize} realSize={$size} zoom={$zoom} />
+      </View>
       <GroupY background={0x333333ff}>
         <Border padding={2}>
           <GroupY gap={2}>
             <colorpicker $color={$color} />
-            <SizeLabels $size={$<Size>({ w: 8, h: 8 })} />
+            <SizeLabels $size={$size} />
             <Button paddingAll={[2, 4]} selected={$grid} onClick={() => $grid.value = !$grid.value}>
               <Label text='grid' />
             </Button>
@@ -48,4 +50,25 @@ function SizeLabels(data: { $size: Ref<Size> }) {
     <Label text='*' color={0xffffff33} />
     <Label text={data.$size.adapt(s => s.h.toString())} />
   </GroupX>
+}
+
+function CanvasView(data: { canvasSize: Ref<Size>, grid: Ref<boolean> }) {
+  return <View size={data.canvasSize} background={0x00000033} />
+}
+
+function Resizer(data: { canvasSize: Ref<Size>, realSize: Ref<Size>, zoom: Ref<number> }) {
+  const point = data.canvasSize.adapt(size => ({ x: size.w, y: size.h }))
+  const view = <View canMouse point={point} background={0x000000aa} size={{ w: 4, h: 4 }} />
+  view.onMouseDown = () => {
+    const $size = $<Size>({ w: $canvasSize.val.w, h: $canvasSize.val.h })
+    $size.watch(size => {
+      data.realSize.value = {
+        w: Math.round(size.w / data.zoom.val),
+        h: Math.round(size.h / data.zoom.val),
+      }
+    })
+    const done = dragResize(sys.$mouse, $size)
+    view.onMouseUp = done
+  }
+  return view
 }
