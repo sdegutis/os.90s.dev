8ebd2a660f61fc28298eb6f87cc0cd4aa1d0d856
sdegutis <sbdegutis@gmail.com>
2025-05-20 12:16:30 -0500

    Cleanup.

--- site/fs/sys/apps/paint.app.tsx
+++ site/fs/sys/apps/paint.app.tsx
@@ -53,22 +53,31 @@ function SizeLabels(data: { $size: Ref<Size> }) {
 }
 
 function CanvasView(data: { canvasSize: Ref<Size>, grid: Ref<boolean> }) {
-  return <View size={data.canvasSize} background={0x00000033} />
+  return <View
+    // draw={function () {
+
+    // }}
+    size={data.canvasSize}
+    background={0x00000033}
+  />
 }
 
 function Resizer(data: { canvasSize: Ref<Size>, realSize: Ref<Size>, zoom: Ref<number> }) {
-  const point = data.canvasSize.adapt(size => ({ x: size.w, y: size.h }))
-  const view = <View canMouse point={point} background={0x000000aa} size={{ w: 4, h: 4 }} />
-  view.onMouseDown = () => {
-    const $size = $<Size>({ w: $canvasSize.val.w, h: $canvasSize.val.h })
-    $size.watch(size => {
-      data.realSize.value = {
-        w: Math.round(size.w / data.zoom.val),
-        h: Math.round(size.h / data.zoom.val),
-      }
-    })
-    const done = dragResize(sys.$mouse, $size)
-    view.onMouseUp = done
-  }
-  return view
+  return <View
+    background={0x000000aa}
+    point={data.canvasSize.adapt(size => ({ x: size.w, y: size.h }))}
+    size={{ w: 4, h: 4 }}
+    canMouse
+    onMouseDown={function () {
+      const $size = $<Size>({ w: $canvasSize.val.w, h: $canvasSize.val.h })
+      $size.watch(size => {
+        data.realSize.value = {
+          w: Math.round(size.w / data.zoom.val),
+          h: Math.round(size.h / data.zoom.val),
+        }
+      })
+      const done = dragResize(sys.$mouse, $size)
+      this.onMouseUp = done
+    }}
+  />
 }
